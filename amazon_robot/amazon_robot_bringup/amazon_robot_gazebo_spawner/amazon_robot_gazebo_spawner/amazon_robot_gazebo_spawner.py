36813c0a7533c8b06bffc2eebb48dcc6fb52d5bc
shreyasgokhale <shreyas6gokhale@gmail.com>
2020-08-07 11:12:22 +0200

    Amazon Robot: Lift + Actuation + Navigation working

--- amazon_robot/amazon_robot_bringup/amazon_robot_gazebo_spawner/amazon_robot_gazebo_spawner/amazon_robot_gazebo_spawner.py
+++ amazon_robot/amazon_robot_bringup/amazon_robot_gazebo_spawner/amazon_robot_gazebo_spawner/amazon_robot_gazebo_spawner.py
@@ -26,10 +26,12 @@ import rclpy
 def main():
     # Get input arguments from user
     parser = argparse.ArgumentParser(description='Spawn Robot into Gazebo with navigation2')
-    parser.add_argument('-n', '--robot_name', type=str, default='robot',
+    parser.add_argument('-n', '--robot_name', type=str, default='amazon_robot',
                         help='Name of the robot to spawn')
-    parser.add_argument('-ns', '--robot_namespace', type=str, default='robot',
+    parser.add_argument('-ns', '--robot_namespace', type=str, default=' ',
                         help='ROS namespace to apply to the tf and plugins')
+    parser.add_argument('-namespace', '--namespace', type=bool, default=False,  
+                        help='Whether to enable namespacing')
     parser.add_argument('-x', type=float, default=0,
                         help='the x component of the initial position [meters]')
     parser.add_argument('-y', type=float, default=0,
@@ -58,10 +60,6 @@ def main():
         client.wait_for_service()
         node.get_logger().info('...connected!')
 
-    node.get_logger().info('spawning `{}` on namespace `{}` at {}, {}, {}'.format(
-        args.robot_name, args.robot_namespace, args.x, args.y, args.z))
-
-
     sdf_file_path = os.path.join(
         get_package_share_directory('amazon_robot_gazebo'), 'models',
         'amazon_robot2', 'model.sdf')
@@ -83,23 +81,36 @@ def main():
     root = tree.getroot()
     for plugin in root.iter('plugin'):
         # TODO(orduno) Handle case if an sdf file from non-turtlebot is provided
-        if 'turtlebot3_diff_drive' in plugin.attrib.values():
+        if 'amazon_robot_diff_drive' in plugin.attrib.values():
             # The only plugin we care for now is 'diff_drive' which is
             # broadcasting a transform between`odom` and `base_footprint`
             break
 
-    ros_params = plugin.find('ros')
-    ros_tf_remap = ET.SubElement(ros_params, 'remapping')
-    ros_tf_remap.text = '/tf:=/' + args.robot_namespace + '/tf'
-
+    
     # Set data for request
     request = SpawnEntity.Request()
     request.name = args.robot_name
     request.xml = ET.tostring(root, encoding='unicode')
-    request.robot_namespace = args.robot_namespace
     request.initial_pose.position.x = float(args.x)
     request.initial_pose.position.y = float(args.y)
     request.initial_pose.position.z = float(args.z)
+
+    if args.namespace is True:     
+        node.get_logger().info('spawning `{}` on namespace `{}` at {}, {}, {}'.format(
+            args.robot_name, args.robot_namespace, args.x, args.y, args.z))
+            
+        ros_params = plugin.find('ros')
+        ros_tf_remap = ET.SubElement(ros_params, 'remapping')
+        ros_tf_remap.text = '/tf:=/' + args.robot_namespace + '/tf'
+        request.robot_namespace = args.robot_namespace
+
+
+    else:
+        node.get_logger().info('spawning `{}` at {}, {}, {}'.format(
+            args.robot_name, args.x, args.y, args.z))
+
+
+    
 
     node.get_logger().info('Sending service request to `/spawn_entity`')
     future = client.call_async(request)
