b7565049cf1b3c52054ecf09e8d0ee7436d61a61
Shreyas Gokhale <shreyas6gokhale@gmail.com>
2020-08-22 15:58:15 +0200

    Amazon Robot: Added working AWS world

--- amazon_robot/start_simulation.sh
+++ amazon_robot/start_simulation.sh
@@ -4,11 +4,14 @@ source /opt/warehouse_ws/install/setup.sh
 
 sleep 2s
 
-ros2 launch slam_toolbox online_async_launch.py &
+#ros2 launch slam_toolbox online_async_launch.py &
+#
+#sleep 2s
 
-sleep 2s
 
-
-ros2 launch amazon_robot_bringup  amazon_warehouse_world.py slam:=True
+#ros2 launch amazon_robot_bringup  amazon_warehouse_world.py slam:=True
 
 #ros2 launch amazon_robot_bringup  amazon_robot_in_tb3_world.py
+
+
+ros2 launch amazon_robot_bringup  amazon_robot_in_aws_world.py
