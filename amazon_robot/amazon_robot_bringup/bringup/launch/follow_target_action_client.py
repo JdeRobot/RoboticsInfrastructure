bdcfe7689411491b0922ecb52dba3af1a2c29dd0
Shreyas Gokhale <shreyas6gokhale@gmail.com>
2020-08-28 01:12:06 +0200

    Multirobot: Exercise client update

--- amazon_robot/amazon_robot_bringup/bringup/launch/follow_target_action_client.py
+++ amazon_robot/amazon_robot_bringup/bringup/launch/follow_target_action_client.py
@@ -74,17 +74,25 @@ Have fun!
 
 
 pallets = {
-    'pallet_0': {'x_pose': 3.62, 'y_pose': 0.42, 'z_pose': 0.01},
-    'pallet_1': {'x_pose': 3.67, 'y_pose': -1.59, 'z_pose': 0.01},
-    'pallet_2': {'x_pose': 3.67, 'y_pose': -3.98, 'z_pose': 0.01},
-}
-storage_locations = {
-    'storage_location_1': {'x_pose': -5.10, 'y_pose': -3.98, 'z_pose': 0.01},
-    'storage_location_2': {'x_pose': -5.14, 'y_pose': 0.45, 'z_pose': 0.01},
+    'pallet_1': {'x_pose': 3.58, 'y_pose': 0.49, 'z_pose': 0.01},
+    'pallet_2': {'x_pose': 3.60, 'y_pose': -1.56, 'z_pose': 0.01},
+    'pallet_3': {'x_pose': 3.61, 'y_pose': -3.29, 'z_pose': 0.01},
+    'pallet_4': {'x_pose': 3.58, 'y_pose': -5.07, 'z_pose': 0.01},
+    'pallet_5': {'x_pose': 3.61, 'y_pose': -6.91, 'z_pose': 0.01},
+    'pallet_6': {'x_pose': 3.72, 'y_pose': -8.88, 'z_pose': 0.01},
 }
 
+storage_locations = {
+    'storage_location_1': {'x_pose': -5.84, 'y_pose': -3.35, 'z_pose': 0.01},
+    'storage_location_2': {'x_pose': -5.84, 'y_pose': 1.12, 'z_pose': 0.01},
+    'storage_location_3': {'x_pose': -5.84, 'y_pose': -7.76, 'z_pose': 0.01},
+}
+
+
+
 free_area = {
-    'end_of_corridor': {'x_pose': 1.35, 'y_pose': -6.78, 'z_pose': 0.01},
+    'right_end_of_corridor': {'x_pose': 1.35, 'y_pose': -6.78, 'z_pose': 0.01},
+    'left_end_of_corridor': {'x_pose': 0.92, 'y_pose': 6.45, 'z_pose': 0.01},
 }
 
 lift_stages = {'load': 2, 'unload': -2, 'half_load': 1, 'half_unload': -1, 'unchanged': 0}
@@ -110,12 +118,20 @@ class WarehouseController:
         pass
 
     def create_test_plan(self):
-        poses_robot1 = [get_pose_stamped(**pallets['pallet_0']),
-                        get_pose_stamped(**storage_locations['storage_location_1'])]
-        loads_robot1 = [lift_stages['load'], lift_stages['unload']]
-        poses_robot2 = [get_pose_stamped(**free_area['end_of_corridor']), get_pose_stamped(**pallets['pallet_1']),
-                        get_pose_stamped(**storage_locations['storage_location_2'])]
-        loads_robot2 = [lift_stages['unchanged'], lift_stages['load'], lift_stages['unload']]
+        poses_robot1 = [get_pose_stamped(**pallets['pallet_1']),
+                        get_pose_stamped(**storage_locations['storage_location_1']),
+                        get_pose_stamped(**pallets['pallet_3']),
+                        get_pose_stamped(**storage_locations['storage_location_3'])
+                        ]
+
+        loads_robot1 = [lift_stages['load'], lift_stages['unload'], lift_stages['load'], lift_stages['unload']]
+        poses_robot2 = [get_pose_stamped(**free_area['right_end_of_corridor']),
+                        get_pose_stamped(**pallets['pallet_2']),
+                        get_pose_stamped(**storage_locations['storage_location_2']),
+                        get_pose_stamped(**pallets['pallet_4']),
+                        get_pose_stamped(**pallets['pallet_1'])]
+
+        loads_robot2 = [lift_stages['unchanged'], lift_stages['load'], lift_stages['unload'], lift_stages['load'], lift_stages['unload']]
         #
         # print("Poses List ")
         # print(poses_robot1)
@@ -138,21 +154,27 @@ class FollowTargetActionClient(Node):
         super().__init__(action_client_name)
         self._action_client = ActionClient(self, FollowTargets, action_name)
 
+
     def get_action_client(self):
         return self._action_client
 
     def send_targets(self, poses, loads):
-        goal_msg = FollowTargets.Goal()
-        goal_msg.poses = poses
-        goal_msg.load = loads
-        self._action_client.wait_for_server()
-        self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self.get_logger().info('Received Goal poses: {0}'.format(len(poses)))
+        self.get_logger().info('Received Load instructions: {0}'.format(len(loads)))
+        if len(poses) == len(loads):
+            goal_msg = FollowTargets.Goal()
+            goal_msg.poses = poses
+            goal_msg.load = loads
+            self.goal_length = len(poses)
+            self._action_client.wait_for_server()
+            self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        else:
+            self.get_logger().warn('Unequal amount of Poses and loads!')
 
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        print(feedback)
-        # self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+        # self.get_logger().info('Currently Executing: {0} out of {0} targets'.format(feedback.current_waypoint, self.goal_length))
 
 
 
