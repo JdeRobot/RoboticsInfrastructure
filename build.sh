3a3e6f33602734f444dc343cd2dbfb7f31f88a9a
Blanca <b.soriar.2019@alumnos.urjc.es>
2023-03-28 10:53:39 +0200

    turbovnc.deb for amd64 in scripts
    Signed-off-by: Blanca <b.soriar.2019@alumnos.urjc.es>

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ $# -ne 1 ] ; then
+    echo usage: ./build.sh \<tag\>
+    exit 1
+fi
+tag=$1
+
 sudo docker build -f scripts/Dockerfile.base -t humble_base .
-sudo docker build --no-cache=true -f scripts/Dockerfile -t jderobot/robotics-academy:4.3.0 .
+sudo docker build --no-cache=true -f scripts/Dockerfile -t jderobot/robotics-academy:$tag .
