bff6f161eee1a06b1250a17cea38a61a6f2c1992
Blanca <b.soriar.2019@alumnos.urjc.es>
2023-03-29 10:50:20 +0200

    VirtualGL and TurboVNC working properly with specified dev
    Signed-off-by: Blanca <b.soriar.2019@alumnos.urjc.es>

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ if [ $# -ne 1 ] ; then
 fi
 tag=$1
 
-sudo docker build -f scripts/Dockerfile.base -t humble_base .
-sudo docker build --no-cache=true -f scripts/Dockerfile -t jderobot/robotics-academy:$tag .
+sudo docker build --rm -f scripts/Dockerfile.base -t humble_base .
+sudo docker build --rm --no-cache=true -f scripts/Dockerfile -t jderobot/robotics-academy:$tag .
