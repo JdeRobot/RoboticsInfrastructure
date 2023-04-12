72e3ff5b8ea40602edf24dfc910cb308fd36b5d0
Blanca <b.soriar.2019@alumnos.urjc.es>
2023-04-12 10:43:27 +0200

    deleted cache files
    Signed-off-by: Blanca <b.soriar.2019@alumnos.urjc.es>

--- tmp/webserver/react_frontend/node_modules/react-vnc/dist/types/noVNC/core/decoders/rre.d.ts
+++
@@ -1,4 +0,0 @@
-export default class RREDecoder {
-    _subrects: number;
-    decodeRect(x: any, y: any, width: any, height: any, sock: any, display: any, depth: any): boolean;
-}
--- tmp/webserver/react_frontend/node_modules/react-vnc/dist/types/noVNC/core/input/util.d.ts
+++
@@ -1,3 +0,0 @@
-export function getKeycode(evt: any): any;
-export function getKey(evt: any): any;
-export function getKeysym(evt: any): any;
