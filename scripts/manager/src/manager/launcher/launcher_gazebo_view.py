0735eacd5b84cc0676673188c76b3d48e6d9c363
lucia <lucia.chen189@gmail.com>
2023-04-24 14:29:06 +0200

    moved sleep of gzserver to gzclient, and updated amazon_hospital and turtlebot2 folders

--- scripts/manager/src/manager/launcher/launcher_gazebo_view.py
+++ scripts/manager/src/manager/launcher/launcher_gazebo_view.py
@@ -20,17 +20,17 @@ class LauncherGazeboView(ILauncher):
         # Configure browser screen width and height for gzclient
         gzclient_config_cmds = f"echo [geometry] > ~/.gazebo/gui.ini; echo x=0 >> ~/.gazebo/gui.ini; echo y=0 >> ~/.gazebo/gui.ini; echo width={self.width} >> ~/.gazebo/gui.ini; echo height={self.height} >> ~/.gazebo/gui.ini;"
         gz_vnc = Vnc_server()
-        
+
         if ACCELERATION_ENABLED:
             gz_vnc.start_vnc_gpu(self.display, self.internal_port, self.external_port,DRI_PATH)
-            time.sleep(0.1)
+            time.sleep(6)
 
             # Write display config and start gzclient
             gzclient_cmd = (
                 f"export DISPLAY=:0; {gzclient_config_cmds} export VGL_DISPLAY={DRI_PATH}; vglrun gzclient --verbose")
         else:
             gz_vnc.start_vnc(self.display, self.internal_port, self.external_port)
-            time.sleep(0.1)
+            time.sleep(6)
 
             gzclient_cmd = (
                 f"export DISPLAY=:0; {gzclient_config_cmds} gzclient --verbose")
