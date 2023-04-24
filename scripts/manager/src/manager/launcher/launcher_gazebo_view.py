ca5e71f9095f6a326f1e14b8dbbbf891e5efe16d
lucia <lucia.chen189@gmail.com>
2023-04-24 18:06:33 +0200

    long sleep only for follow person exercise

--- scripts/manager/src/manager/launcher/launcher_gazebo_view.py
+++ scripts/manager/src/manager/launcher/launcher_gazebo_view.py
@@ -5,7 +5,9 @@ import time
 import os
 import stat
 
+
 class LauncherGazeboView(ILauncher):
+    exercise_id: str
     display: str
     internal_port: str
     external_port: str
@@ -14,7 +16,8 @@ class LauncherGazeboView(ILauncher):
     running = False
 
     def run(self, callback):
-        DRI_PATH = os.path.join("/dev/dri", os.environ.get("DRI_NAME", "card0"))
+        DRI_PATH = os.path.join(
+            "/dev/dri", os.environ.get("DRI_NAME", "card0"))
         ACCELERATION_ENABLED = self.check_device(DRI_PATH)
 
         # Configure browser screen width and height for gzclient
@@ -22,19 +25,30 @@ class LauncherGazeboView(ILauncher):
         gz_vnc = Vnc_server()
 
         if ACCELERATION_ENABLED:
-            gz_vnc.start_vnc_gpu(self.display, self.internal_port, self.external_port,DRI_PATH)
-            time.sleep(6)
+            gz_vnc.start_vnc_gpu(
+                self.display, self.internal_port, self.external_port, DRI_PATH)
 
+            if (self.exercise_id == "follow_person_newmanager"):
+                print("\n\n SLEEEP \n\n")
+                time.sleep(6)
+            else:
+                time.sleep(0.1)
             # Write display config and start gzclient
             gzclient_cmd = (
                 f"export DISPLAY=:0; {gzclient_config_cmds} export VGL_DISPLAY={DRI_PATH}; vglrun gzclient --verbose")
         else:
-            gz_vnc.start_vnc(self.display, self.internal_port, self.external_port)
-            time.sleep(6)
+            gz_vnc.start_vnc(self.display, self.internal_port,
+                             self.external_port)
+
+            if (self.exercise_id == "follow_person_newmanager"):
+                print("\n\n SLEEEP \n\n")
+                time.sleep(6)
+            else:
+                time.sleep(0.1)
 
             gzclient_cmd = (
                 f"export DISPLAY=:0; {gzclient_config_cmds} gzclient --verbose")
-            
+
         gzclient_thread = DockerThread(gzclient_cmd)
         gzclient_thread.start()
         self.running = True
@@ -44,7 +58,7 @@ class LauncherGazeboView(ILauncher):
             return stat.S_ISCHR(os.lstat(device_path)[stat.ST_MODE])
         except:
             return False
-        
+
     def is_running(self):
         return self.running
 
