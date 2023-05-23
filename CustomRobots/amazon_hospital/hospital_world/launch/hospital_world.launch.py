bbc9f4fadf867d867fda0e0feb70430253d0fa78
Blanca <blancasoriaru@gmail.com>
2023-05-23 12:34:40 +0200

    fixed model paths
    Signed-off-by: Blanca <blancasoriaru@gmail.com>

--- CustomRobots/amazon_hospital/hospital_world/launch/hospital_world.launch.py
+++ CustomRobots/amazon_hospital/hospital_world/launch/hospital_world.launch.py
@@ -5,11 +5,21 @@ import launch
 from launch.conditions import IfCondition
 from launch.substitutions import PythonExpression
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+
+    # Set the path to the Turtlebot2 ROS package
+    pkg_share_dir = FindPackageShare(package='custom_robots').find('custom_robots')
+
     world_file_name = "hospital.world"
-    world = os.path.join(get_package_share_directory('hospital_world'), 'worlds', world_file_name)
+    world = os.path.join(pkg_share_dir, 'worlds', world_file_name)
+
+    gazebo_models_path = os.path.join(pkg_share_dir, 'models')
+    gazebo_fuel_models_path = os.path.join(pkg_share_dir, 'fuel_models')
+    os.environ["GAZEBO_MODEL_PATH"] = f"{os.environ.get('GAZEBO_MODEL_PATH', '')}:{':'.join(gazebo_models_path)}"
+    os.environ["GAZEBO_MODEL_PATH"] = f"{os.environ.get('GAZEBO_MODEL_PATH', '')}:{':'.join(gazebo_fuel_models_path)}"
 
     gazebo_ros = get_package_share_directory('gazebo_ros')
     gazebo_client = launch.actions.IncludeLaunchDescription(
