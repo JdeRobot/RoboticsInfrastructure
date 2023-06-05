2e689011a844507ccab53c9827f6d9faacc6fd90
Blanca <blancasoriaru@gmail.com>
2023-06-05 12:14:58 +0200

    model_teleoperator log file and updates
    Signed-off-by: Blanca <blancasoriaru@gmail.com>

--- utils/model_teleoperator.py
+++ utils/model_teleoperator.py
@@ -6,55 +6,65 @@ import time
 
 class ModelTeleoperator:
 
-    def __init__(self, model_port):
+    def __init__(self):
         self.server = None
         self.client = None
         self.host = sys.argv[1]
 
-        self.model_address = ("127.0.0.1", model_port)
+        self.model_address = ("127.0.0.1", 36677)
         self.model_client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
     # Function to read the message from websocket
     def get_message(self, client, server, message):
-        if (message[:7] == "#teleop"):
-            mode = message[message.find('_')+1:]
-            
-            print("\n\nTeleoperate mode: " + str(mode) + "\n\n")
-            
-            if mode == "true":
-                self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop
-            elif mode == "false":
-                self.model_client.sendto(str.encode("A--"), self.model_address) # Autonomous
+        try:
+            sys.stdout = open("teleop_log.txt","w")
+            if (message[:7] == "#teleop"):
+                mode = message[message.find('_')+1:]
+                print("\n\nTeleoperate mode: " + str(mode) + "\n\n")
+                if mode == "true":
+                    self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop
+                elif mode == "false":
+                    self.model_client.sendto(str.encode("A--"), self.model_address) # Autonomous
 
-        elif (message[:4] == "#key"):
-            mode = message[message.find('_')+1:]
-            
-            print("\nKey " + str(mode) +"\n")
-            
-            if mode == "w":
-                self.model_client.sendto(str.encode("UVF"), self.model_address) # User Velocity Forward
-            elif mode == "s":
-                self.model_client.sendto(str.encode("UVB"), self.model_address) # User Velocity Backward
-            elif mode == "a":
-                self.model_client.sendto(str.encode("UAL"), self.model_address) # User Angular Left
-            elif mode == "d":
-                self.model_client.sendto(str.encode("UAR"), self.model_address) # User Angular Right
-            elif mode == "x":
-                self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop model
+            elif (message[:4] == "#key"):
+                mode = message[message.find('_')+1:]
+                print("\nKey " + str(mode) +"\n")
+                if mode == "w":
+                    self.model_client.sendto(str.encode("UVF"), self.model_address) # User Velocity Forward
+                elif mode == "s":
+                    self.model_client.sendto(str.encode("UVB"), self.model_address) # User Velocity Backward
+                elif mode == "a":
+                    self.model_client.sendto(str.encode("UAL"), self.model_address) # User Angular Left
+                elif mode == "d":
+                    self.model_client.sendto(str.encode("UAR"), self.model_address) # User Angular Right
+                elif mode == "x":
+                    self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop model
 
+        except KeyboardInterrupt:
+            sys.stdout.close()  # close file.txt
+            sys.stdout = sys.__stdout__
 
     # Function that gets called when the connected closes
     def handle_close(self, client, server):
-        self.client = None
-        self.server.allow_new_connections()
-        print(client, 'closed')
+        try:
+            sys.stdout = open("teleop_log.txt","w")
+            self.client = None
+            self.server.allow_new_connections()
+            print(client, 'closed')
+        except KeyboardInterrupt:
+            sys.stdout.close()  # close file.txt
+            sys.stdout = sys.__stdout__
 
     # Called when a new client is received
     def get_client(self, client, server):
-        self.client = client
-        self.server.deny_new_connections()
-        print(client, 'connected')
-
+        try:
+            sys.stdout = open("teleop_log.txt","w")
+            self.client = client
+            self.server.deny_new_connections()
+            print(client, 'connected')
+        except KeyboardInterrupt:
+            sys.stdout.close()  # close file.txt
+            sys.stdout = sys.__stdout__
 
     # Activate the server
     def run_server(self):
@@ -76,11 +86,12 @@ class ModelTeleoperator:
                 print("~/ws_teleop.log could not be opened for write", flush=True)
                 time.sleep(0.1)
 
+        print("\n\n RUN FOREVER TELEOPERATOR!!! ")
         self.server.run_forever()
 
 
 # Execute!
 if __name__ == "__main__":
-    model_plugin_port = sys.argv[2]
-    server = ModelTeleoperator(model_plugin_port)
+    print("\n\n MAIN!!! ")
+    server = ModelTeleoperator()
     server.run_server()
