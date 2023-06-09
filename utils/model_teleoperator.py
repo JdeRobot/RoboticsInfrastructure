fa025afbf3f524fa0fc998b7841abd6456064ef0
Blanca <blancasoriaru@gmail.com>
2023-06-09 14:31:19 +0200

    model_teleopertor only to teleoperator
    Signed-off-by: Blanca <blancasoriaru@gmail.com>

--- utils/model_teleoperator.py
+++ utils/model_teleoperator.py
@@ -6,10 +6,10 @@ import time
 
 class ModelTeleoperator:
 
-    def __init__(self):
+    def __init__(self, host):
         self.server = None
         self.client = None
-        self.host = sys.argv[1]
+        self.host = host
 
         self.model_address = ("127.0.0.1", 36677)
         self.model_client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -17,27 +17,19 @@ class ModelTeleoperator:
     # Function to read the message from websocket
     def get_message(self, client, server, message):
         try:
-            sys.stdout = open("teleop_log.txt","w")
-            if (message[:7] == "#teleop"):
-                mode = message[message.find('_')+1:]
-                print("\n\nTeleoperate mode: " + str(mode) + "\n\n")
-                if mode == "true":
-                    self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop
-                elif mode == "false":
-                    self.model_client.sendto(str.encode("A--"), self.model_address) # Autonomous
-
-            elif (message[:4] == "#key"):
-                mode = message[message.find('_')+1:]
-                print("\nKey " + str(mode) +"\n")
-                if mode == "w":
+            sys.stdout = open("/teleop_log.txt","w")
+            if (message[:4] == "#key"):
+                key = message[message.find('_')+1:]
+                print("\nPRESSED KEY:" + str(key))
+                if key == "w":
                     self.model_client.sendto(str.encode("UVF"), self.model_address) # User Velocity Forward
-                elif mode == "s":
+                elif key == "s":
                     self.model_client.sendto(str.encode("UVB"), self.model_address) # User Velocity Backward
-                elif mode == "a":
+                elif key == "a":
                     self.model_client.sendto(str.encode("UAL"), self.model_address) # User Angular Left
-                elif mode == "d":
+                elif key == "d":
                     self.model_client.sendto(str.encode("UAR"), self.model_address) # User Angular Right
-                elif mode == "x":
+                elif key == "x":
                     self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop model
 
         except KeyboardInterrupt:
@@ -46,10 +38,10 @@ class ModelTeleoperator:
 
     # Function that gets called when the connected closes
     def handle_close(self, client, server):
+        self.client = None
+        self.server.allow_new_connections()
         try:
-            sys.stdout = open("teleop_log.txt","w")
-            self.client = None
-            self.server.allow_new_connections()
+            sys.stdout = open("/teleop_log.txt","w")
             print(client, 'closed')
         except KeyboardInterrupt:
             sys.stdout.close()  # close file.txt
@@ -57,14 +49,17 @@ class ModelTeleoperator:
 
     # Called when a new client is received
     def get_client(self, client, server):
+        self.client = client
+        self.server.deny_new_connections()
+        self.model_client.sendto(str.encode("US-"), self.model_address) # User Stop
         try:
-            sys.stdout = open("teleop_log.txt","w")
-            self.client = client
-            self.server.deny_new_connections()
+            sys.stdout = open("/teleop_log.txt","w")
             print(client, 'connected')
         except KeyboardInterrupt:
+            print(client, 'error connecting with teleoperator client')
             sys.stdout.close()  # close file.txt
             sys.stdout = sys.__stdout__
+
 
     # Activate the server
     def run_server(self):
@@ -86,12 +81,11 @@ class ModelTeleoperator:
                 print("~/ws_teleop.log could not be opened for write", flush=True)
                 time.sleep(0.1)
 
-        print("\n\n RUN FOREVER TELEOPERATOR!!! ")
         self.server.run_forever()
 
 
 # Execute!
 if __name__ == "__main__":
-    print("\n\n MAIN!!! ")
-    server = ModelTeleoperator()
+    host = sys.argv[1]
+    server = ModelTeleoperator(host)
     server.run_server()
