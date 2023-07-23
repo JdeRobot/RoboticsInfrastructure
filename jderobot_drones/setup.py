8d506d1390528bb8426efd32a92f8e351d51ac4a
pawanw17 <pawanw17@gmail.com>
2023-07-23 12:07:02 +0530

    updated meta data

--- jderobot_drones/setup.py
+++ jderobot_drones/setup.py
@@ -14,8 +14,8 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
+    maintainer_email='pawanw17@gmail.com',
+    description='Jde Robot drones package to integrate Aerostack2 and pixhawk with Robotics Academy',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
