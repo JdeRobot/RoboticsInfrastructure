87a5110a41a502641ae864658d143a734463c455
ReyDoran <manetoscopio@gmail.com>
2023-10-06 11:39:19 +0200

    Entrypoint setup restored

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -34,7 +34,7 @@ if [ $webserver == true ]; then
 else
     runserver=""
 fi
-
+ros_setup=" source /.env && source ~/.bashrc && source /home/ws/install/setup.bash; "
 runmanager="python3 RoboticsAcademy/manager/manager.py"
 runram="python3 RoboticsAcademy/src/manager/manager/manager.py 0.0.0.0 7163"
 root="cd /"
@@ -43,9 +43,11 @@ root="cd /"
 if [ $log == true ]; then
     DATE_TIME=$(date +%F-%H-%M) # FORMAT year-month-date-hours-mins
     mkdir -p /root/.roboticsacademy/log/$DATE_TIME/
+    eval ${ros_setup}
     script -q -c "$root & $runserver & $runram ;" /root/.roboticsacademy/log/$DATE_TIME/manager.log
     cp -r /root/.ros/log/* /root/.roboticsacademy/log/$DATE_TIME
 else
+    eval ${ros_setup}
     if [ $debug == true ]; then
       { bash ; }
     else
