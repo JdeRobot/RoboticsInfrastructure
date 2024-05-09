add133303ef571e42f8fdd99f4da5faba1d558a1
Óscar Martínez <oscar.personal@tutanota.com>
2024-05-09 21:38:35 +0200

    New bumper API

--- hal_interfaces/hal_interfaces/general/bumper.py
+++ hal_interfaces/hal_interfaces/general/bumper.py
@@ -3,70 +3,39 @@ from gazebo_msgs.msg import ContactsState
 from functools import partial
 
 
-### AUXILIARY FUNCTIONS ###
-class BumperData:
-
-    def __init__(self):
-
-        self.pressed_bumpers = []  # Indicates which bumpers are pressed
-        self.timeStamp = 0  # Time stamp [s]
-
-    def __str__(self):
-        s = "BumperData: {\n   pressed bumpers: " + str(self.bumpers)
-        s = s + "\n   timeStamp: " + str(self.timeStamp) + "\n}"
-        return s
-
-
-def contactsStateList2BumperData(bumper_msg_list_):
-    """
-    Translates from ROS ContactsState to JderobotTypes BumperData.
-    @param event: ROS ContactsState to translate
-    @type event: ContactsState
-    @return a BumperData translated from event
-
-    # bumpers
-    Same order as in the topics list
-    """
-    bump = BumperData()
-    latest_timestamp = 0
-    n_bumpers_ = len(bumper_msg_list_)
-
-    # Add the pressed bumpers, calculating the latest timestamp
-    for i in range(n_bumpers_):
-        if len(bumper_msg_list_[i].states) > 0:
-            bump.pressed_bumpers.append(i)
-
-        msg_timestamp = bumper_msg_list_[i].header.stamp.sec + (
-            bumper_msg_list_[i].header.stamp.nanosec * 1e-9
-        )
-        latest_timestamp = max(latest_timestamp, msg_timestamp)
-
-    bump.timeStamp = latest_timestamp
-
-    return bump
-
-
 ### HAL INTERFACE ###
 class BumperNode(Node):
 
     def __init__(self, topics):
         super().__init__("bumper_node")
+
+        # Hardcoded for the moment to three topics
+        # as dynamic callback creation is not trivial
         self.topics = topics
-        self.subs = []
-        self.n_bumpers_ = len(topics)
-        self.last_bumper_msgs_ = [ContactsState() for _ in range(self.n_bumpers_)]
+        self.callbacks_ = [
+            self.right_callback,
+            self.center_callback,
+            self.left_callback,
+        ]
 
-        for i in range(self.n_bumpers_):
-            sub = self.create_subscription(
-                ContactsState,
-                self.topics[i],
-                partial(self.callback, index=i),
-                10,
+        # Subscribe to all the callbacks
+        for i in range(len(self.topics)):
+
+            self.sub = self.create_subscription(
+                ContactsState, topics[i], self.callbacks_[i], 10
             )
-            self.subs.append(sub)
 
-    def callback(self, msg, index):
-        self.last_bumper_msgs_[index] = msg
+        # Right, center, left
+        self.contact_states_ = [ContactsState() for _ in range(3)]
 
-    def getBumperData(self):
-        return contactsStateList2BumperData(self.last_bumper_msgs_)
+    def right_callback(self, contact):
+        self.contact_states_[0] = contact
+
+    def center_callback(self, contact):
+        self.contact_states_[1] = contact
+
+    def left_callback(self, contact):
+        self.contact_states_[1] = contact
+
+    def get_contact(self, index):
+        return self.contact_states_[index]
