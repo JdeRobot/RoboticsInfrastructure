6ba6e977cca50ba16afc0c63b591a6a030e31894
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-10-24 19:30:53 +0200

    Update db

--- universes.sql
+++ universes.sql
@@ -5,27 +5,27 @@
 -- Dumped from database version 16.4 (Ubuntu 16.4-0ubuntu0.24.04.2)
 -- Dumped by pg_dump version 16.4 (Ubuntu 16.4-0ubuntu0.24.04.2)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+-- SET statement_timeout = 0;
+-- SET lock_timeout = 0;
+-- SET idle_in_transaction_session_timeout = 0;
+-- SET client_encoding = 'UTF8';
+-- SET standard_conforming_strings = on;
+--SELECT pg_catalog.set_config('search_path', '', false);
+--SET check_function_bodies = false;
+--SET xmloption = content;
+--SET client_min_messages = warning;
+--SET row_security = off;
 
-SET default_tablespace = '';
+--SET default_tablespace = '';
 
-SET default_table_access_method = heap;
+--SET default_table_access_method = heap;
 
 --
 -- Name: universes; Type: TABLE; Schema: public; Owner: admin
 --
 
-CREATE TABLE public.universes (
-    id integer NOT NULL,
+CREATE TABLE exercises_world (
+    id integer PRIMARY KEY,
     name character varying(100) NOT NULL,
     launch_file_path character varying(200) NOT NULL,
     ros_version character varying(4) NOT NULL,
@@ -33,14 +33,21 @@ CREATE TABLE public.universes (
     world character varying(50) NOT NULL
 );
 
+CREATE TABLE exercises_exercise_worlds (
+    id integer PRIMARY KEY,
+    exercise_id bigint NOT NULL,
+    world_id integer REFERENCES exercises_world (id)
+);
 
-ALTER TABLE public.universes OWNER TO admin;
+
+
+-- ALTER TABLE public.universes OWNER TO user-dev;
 
 --
 -- Data for Name: universes; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY public.universes (id, name, launch_file_path, ros_version, visualization, world) FROM stdin;
+COPY exercises_world (id, name, launch_file_path, ros_version, visualization, world) FROM stdin;
 1	autoparking_ros1	/opt/jderobot/Launchers/autoparking.launch	ROS1	gazebo_rae	gazebo
 2	basic_vacuum_cleaner_ros2	/opt/jderobot/Launchers/vacuum_cleaner.launch.py	ROS2	gazebo_rae	gazebo
 3	obstacle_avoidance_ros1	/opt/jderobot/Launchers/obstacle_avoidance_f1_headless.launch	ROS1	gazebo_rae	gazebo
@@ -74,13 +81,47 @@ COPY public.universes (id, name, launch_file_path, ros_version, visualization, w
 31	follow_line_default_ack_ros2	/opt/jderobot/Launchers/simple_circuit_ackermann.launch.py	ROS2	gazebo_rae	gazebo
 \.
 
+COPY exercises_exercise_worlds (id, exercise_id, world_id) FROM stdin;
+1	52	1
+3	51	2
+5	56	4
+6	57	5
+8	58	7
+10	69	8
+13	50	10
+14	50	9
+15	50	11
+16	55	12
+17	55	13
+18	52	14
+19	71	15
+20  	59	16
+21	61	18
+22	52	19
+23	52	20
+24	52	21
+25	52	22
+26	69	24
+27	69	25
+28	69	23
+29	50	26
+30	50	27
+31	50	28
+32	50	29
+33	50	30
+34	50	31
+\.
+
 
 --
 -- Name: universes universes_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY public.universes
-    ADD CONSTRAINT universes_pkey PRIMARY KEY (id);
+-- ALTER TABLE ONLY public.exercises_world
+    -- ADD CONSTRAINT exercises_world_pkey PRIMARY KEY (id);
+    
+-- ALTER TABLE ONLY public.exercises_exercise_world
+    -- ADD CONSTRAINT exercises_exercise_world_pkey PRIMARY KEY (id);
 
 
 --
