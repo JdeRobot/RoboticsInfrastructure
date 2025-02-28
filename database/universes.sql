104d9fc9929c19aa48ab65504984bd9f6c4b4d41
JuanFernandezEva <j.fernandezev.2018@alumnos.urjc.es>
2025-02-28 10:39:24 +0100

    Changed gui config and world ground

--- database/universes.sql
+++ database/universes.sql
@@ -189,7 +189,7 @@ COPY public.worlds (id, name, launch_file_path, visualization_config_path, ros_v
 30	Warehouse 2 Ackermann	/opt/jderobot/Launchers/pallet_warehouse_with_ackermann_logistic_robot.launch.py	None	ROS2	gazebo_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 31	Rescue People Harmonic	/opt/jderobot/Launchers/rescue_people.launch.py	/opt/jderobot/Launchers/visualization/rescue_people.config	ROS2	gzsim_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 32	Follow Road Harmonic	/opt/jderobot/Launchers/follow_road.launch.py	/opt/jderobot/Launchers/visualization/follow_road.config	ROS2	gzsim_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
-33	Small Laser Mapping Warehouse	/opt/jderobot/Launchers/small_laser_mapping.launch.py	/opt/jderobot/Launchers/visualization/laser_mapping.config	ROS2	gzsim_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
+33	Small Laser Mapping Warehouse	/opt/jderobot/Launchers/small_laser_mapping.launch.py	/opt/jderobot/Launchers/visualization/small_laser_mapping.config	ROS2	gzsim_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 \.
 
 --
