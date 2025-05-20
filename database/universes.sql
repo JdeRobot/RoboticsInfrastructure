a47b8624274b12ca3630398980d302573e7f242b
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-05-20 16:11:56 +0200

    Add demo world to db

--- database/universes.sql
+++ database/universes.sql
@@ -148,6 +148,7 @@ COPY public.universes (id, name, world_id, robot_id) FROM stdin;
 32	Follow Road Harmonic	32	0
 33	Laser Mapping Warehouse	12	0
 34	Pick And Place World	34	0
+35	O3DE Demo World	35	0
 \.
 
 
@@ -192,6 +193,7 @@ COPY public.worlds (id, name, launch_file_path, visualization_config_path, ros_v
 32	Follow Road Harmonic	/opt/jderobot/Launchers/follow_road.launch.py	/opt/jderobot/Launchers/visualization/follow_road.config	ROS2	gzsim_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 33	Small Laser Mapping Warehouse	/opt/jderobot/Launchers/small_laser_mapping.launch.py	/opt/jderobot/Launchers/visualization/small_laser_mapping.config	ROS2	gzsim_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 34	Pick And Place Arm	/opt/jderobot/Launchers/pick_place.launch.py	None	ROS2	gazebo_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
+35	O3DE Demo	/opt/jderobot/Launchers/pick_place.launch.py	None	ROS2	gazebo_rae	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 \.
 
 --
