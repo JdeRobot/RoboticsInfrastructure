fdc0e7ddab52403c39ac846e6b508f38f52b9018
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-06-26 10:58:52 +0200

    Fix db

--- Launchers/monaco_circuit.launch.py
+++ Launchers/monaco_circuit.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     pkg_share = FindPackageShare(package="custom_robots").find("custom_robots")
 
     # Set the path to the world file
-    world_file_name = "spa_circuit.world"
+    world_file_name = "monaco_circuit.world"
     worlds_dir = "/opt/jderobot/Worlds"
     world_path = os.path.join(worlds_dir, world_file_name)
 
