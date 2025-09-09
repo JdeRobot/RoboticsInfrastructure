4371fe6694186e1f88c7d07b349f7b216339e4c1
Astha Sahu <134774307+aastha51551@users.noreply.github.com>
2025-09-10 02:46:37 +0530

    Update car_junction.launch.py

--- Launchers/car_junction.launch.py
+++ Launchers/car_junction.launch.py
@@ -27,6 +27,7 @@ def generate_launch_description():
     package_dir = get_package_share_directory("custom_robots")
     ros_gz_sim = get_package_share_directory("ros_gz_sim")
     use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+    gazebo_models_path = os.path.join(package_dir, "models")
     world_file_name = "road_junction.world"
     worlds_dir = "/opt/jderobot/Worlds"
     world_path = os.path.join(worlds_dir, world_file_name)
@@ -57,31 +58,30 @@ def generate_launch_description():
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            "/clock",
-            "/cmd_vel", 
-            "/odom",
-            "/waymo/lidar/points",
-            "/waymo/camera_info",
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist", 
+            "/odom@nav_msgs/msg/Odometry]gz.msgs.Odometry",
+            "/waymo/lidar/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
         ],
         parameters=[{'use_sim_time': True}],
         output="screen",
     )
     
-    # Set the path to the SDF model files.
-    gazebo_models_path = os.path.join(pkg_share, "models")
-    os.environ["GAZEBO_MODEL_PATH"] = (
-        f"{os.environ.get('GAZEBO_MODEL_PATH', '')}:{':'.join(gazebo_models_path)}"
-    )
 
     start_ros_gazebo_image_bridge = Node(
         package="ros_gz_image",
         executable="image_bridge",
-        arguments=["/waymo/camera_front@sensor_msgs/msg/Image[gz.msgs.Image"],
+        arguments=["/waymo/camera_front"],
         output="screen",
     )
 
     # Create the launch description and populate
     ld = LaunchDescription()
+    ld.add_action(SetEnvironmentVariable("GZ_SIM_RESOURCE_PATH", gazebo_models_path))
+    set_env_vars_resources = AppendEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", os.path.join(package_dir, "models")
+    )
+    ld.add_action(set_env_vars_resources)
     ld.add_action(gazebo_server)
     ld.add_action(world_entity_cmd)
     ld.add_action(start_ros_gazebo_bridge)
