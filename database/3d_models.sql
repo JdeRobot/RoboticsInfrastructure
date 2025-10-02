8702351d632b4dd20411bb3081a590ffdc87ced2
miniesda <miniesda@gmail.com>
2025-10-02 14:33:06 +0200

    Update 3d_models.sql

--- database/3d_models.sql
+++ database/3d_models.sql
@@ -90,6 +90,7 @@ COPY public.robot_viewer (id, name, folder_path) FROM stdin;
 8	Turtlebot3	../../../../static/RoboticsAcademy/RoboticsInfrastructure/CustomRobots/turtlebot3/urdf/turtlebot3_waffle.urdf
 9	Typhoon h480 dual cam	../../../../static/RoboticsAcademy/RoboticsInfrastructure/CustomRobots/drone_assets/models/typhoon_h480_dual_cam/typhoon.urdf
 10	Ackermann Logistic Robot	../../../../static/RoboticsAcademy/RoboticsInfrastructure/CustomRobots/ackermann_logistic_robot/models/ackermann_logistic_robot/ackermannLogisticRobot.urdf
+11	Roomba	../../../../static/RoboticsAcademy/RoboticsInfrastructure/CustomRobots/roomba_robot/models/roombaROS/roomba.glb
 \.
 
 --2	Roomba	../../../../static/RoboticsAcademy/RoboticsInfrastructure/CustomRobots/roomba_robot/models/roombaROS/roombaROS.urdf
