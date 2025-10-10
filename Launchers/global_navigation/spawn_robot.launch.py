5cd2151c16bda805758b92cb8c379b214ee5c0e3
Alberto León Luengo <a.leon.2020@alumnos.urjc.es>
2025-10-10 17:45:38 +0200

    Update spawn_robot.launch.py

--- Launchers/global_navigation/spawn_robot.launch.py
+++ Launchers/global_navigation/spawn_robot.launch.py
@@ -23,7 +23,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get the urdf file
-    model_folder = "turtlebot3_waffle"
+    model_folder = "taxi_holo_ROS_harmonic"
     urdf_path = os.path.join(
         get_package_share_directory("custom_robots"),
         "models",
