1e46279897f54a290d13ec2d995840d7be1205cb
Alberto León Luengo <a.leon.2020@alumnos.urjc.es>
2025-10-24 09:14:55 +0200

    Changing waymo for prius car

--- Launchers/autopark_line/spawn_robot.launch.py
+++ Launchers/autopark_line/spawn_robot.launch.py
@@ -23,7 +23,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get the urdf file
-    model_folder = "waymo"
+    model_folder = "prius_autoparking_3laser_harmonic"
     urdf_path = os.path.join(
         get_package_share_directory("custom_robots"),
         "models",
@@ -63,7 +63,7 @@ def generate_launch_description():
     # )
 
     bridge_params = os.path.join(
-        get_package_share_directory("custom_robots"), "params", "waymo.yaml"
+        get_package_share_directory("custom_robots"), "params", "prius_autoparking_3laser_harmonic.yaml"
     )
 
     start_gazebo_ros_bridge_cmd = Node(
