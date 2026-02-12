dc2bbd6a6ad807fc5ed994355e59e582b0628e68
Carlos <c.iglesias.2018@alumnos.urjc.es>
2026-02-12 19:12:10 +0100

    Adding launcher and ros_gz_bridge

---
+++ Launchers/rover_spawn.launch.py
@@ -0,0 +1,77 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    ros_gz_sim_pkg = get_package_share_directory("ros_gz_sim")
+    custom_share = get_package_share_directory("custom_robots")
+
+    urdf_file = os.path.join(
+        custom_share, "rover_4wd_description", "urdf", "rover_4wd.urdf"
+    )
+
+    world_file = os.path.join(
+        custom_share, "rover_4wd_description", "worlds", "my_warehouse.sdf"
+    )
+
+    bridge_yaml = os.path.join(
+        custom_share, "rover_4wd_description", "config", "ros_gz_bridge.yaml"
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+
+    robot_description = ParameterValue(
+        Command(["xacro", " ", urdf_file]),
+        value_type=str,
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{
+            "use_sim_time": use_sim_time,
+            "robot_description": robot_description
+        }],
+    )
+
+    gz_sim_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim_pkg, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": world_file}.items(),
+    )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-topic", "/robot_description",
+            "-name", "rover_4wd",
+            "-allow_renaming", "true",
+            "-z", "0.15",
+        ],
+        output="screen",
+    )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["--ros-args", "-p", f"config_file:={bridge_yaml}"],
+        output="screen",
+    )
+
+    return LaunchDescription([
+        gz_sim_launch,
+        robot_state_publisher_node,
+        gz_spawn_entity,
+        gz_ros2_bridge,
+    ])
