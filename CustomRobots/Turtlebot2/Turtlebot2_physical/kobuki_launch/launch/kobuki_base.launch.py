6fc56d9d7f451ef0f107590913482702b9e57487
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-02-12 18:21:19 +0100

    Add instructions

--- CustomRobots/Turtlebot2/Turtlebot2_physical/kobuki_launch/launch/kobuki_base.launch.py
+++ CustomRobots/Turtlebot2/Turtlebot2_physical/kobuki_launch/launch/kobuki_base.launch.py
@@ -3,10 +3,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import yaml
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    shared_dir = get_package_share_directory("custom_robots")
+    shared_dir = FindPackageShare(package="custom_robots").find("custom_robots")
 
     params_file = os.path.join(shared_dir, "config", "kobuki_node_params.yaml")
     with open(params_file, "r") as f:
