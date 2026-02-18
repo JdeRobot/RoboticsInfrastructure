6b112098e8b7362bb3355103a644e9d2ae4b267d
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-02-18 13:59:51 +0100

    Change entrypoint back

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -62,7 +62,7 @@ if [ $webserver == true ]; then
     runserver="python3 /RoboticsAcademy/manage.py runserver 0.0.0.0:7164"
 fi
 
-runram="python3 RoboticsApplicationManager/launch.py 0.0.0.0 7163"
+runram="python3 RoboticsApplicationManager/robotics_application_manager/manager/manager.py 0.0.0.0 7163"
 root="cd /"
 
 # TEST LOGS
