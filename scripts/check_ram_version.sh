febeeb472e4422696f3acd2e78c9e2b7bded340d
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-02-20 15:34:31 +0100

    Finish update script with test.pypi

--- scripts/check_ram_version.sh
+++ scripts/check_ram_version.sh
@@ -2,18 +2,32 @@
 
 # Function to get the GPU device path based on vendor priority
 get_curr_version() {
-    echo "Current version: $(pip show robotics_application_manager | egrep -o '([0-9]+\.){2}[0-9]+' | head -n 1)"
+    curr=$(pip show robotics_application_manager | egrep -o '([0-9]+\.){2}[0-9]+' | head -n 1)
+    echo "Current version: $curr"
+    return 0
+}
+
+get_running_version() {
+    echo "Launched version: $(pip show robotics_application_manager | egrep -o '([0-9]+\.){2}[0-9]+' | head -n 1)"
     return 0
 }
 
 get_latest_version() {
-    echo "Latest version: $(pip index versions --index-url https://test.pypi.org/simple/ robotics_application_manager 2>/dev/null | egrep -o '([0-9]+\.){2}[0-9]+' | head -n 1)"
+    latest=$(pip index versions --retries 1 --index-url https://test.pypi.org/simple/ robotics_application_manager 2>/dev/null | egrep -o '([0-9]+\.){2}[0-9]+' | head -n 1)
+    echo "Latest version: $latest"
     return 0
 }
 
 
-# Get the GPU device path based on priority and set DRI_NAME
+# Get the Robotics Application Manager and update it if needed
 echo -e "\n--- Robotics Application Manager info ---"
 get_curr_version
 get_latest_version
+if [[ "$curr" != "$latest" ]];then
+    echo "Upgrading version:"
+    python3 -m pip install --upgrade --retries 1 --index-url https://test.pypi.org/simple/ robotics_application_manager
+else
+    echo "Already on latest version"
+fi
+get_running_version
 echo -e "-------------------------------------------\n"
