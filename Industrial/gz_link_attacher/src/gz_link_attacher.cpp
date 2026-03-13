50c1ed40db9ca37c948010d968ae2ca54f4eff73
jepear19 <jesusper2010@gmail.com>
2026-03-13 11:47:00 +0100

    Fix gz_link_attacher.cpp

--- Industrial/gz_link_attacher/src/gz_link_attacher.cpp
+++ Industrial/gz_link_attacher/src/gz_link_attacher.cpp
@@ -1,11 +1,12 @@
 #include <gz/sim/System.hh>
-#include <gz/sim/Model.hh>
 #include <gz/sim/Link.hh>
+#include <gz/sim/Model.hh>
 
-#include <gz/sim/components/Joint.hh>
-#include <gz/sim/components/JointType.hh>
 #include <gz/sim/components/Name.hh>
 #include <gz/sim/components/ParentEntity.hh>
+#include <gz/sim/components/Joint.hh>
+#include <gz/sim/components/JointType.hh>
+#include <gz/sim/components/JointChild.hh>
 
 #include <gz/plugin/Register.hh>
 
@@ -28,7 +29,6 @@ class LinkAttacher :
 
 public:
 
-///////////////////////////////////////////////////////////////
 
   void Configure(
       const Entity &_entity,
@@ -45,11 +45,6 @@ public:
     node = std::make_shared<rclcpp::Node>("gz_link_attacher");
 
     executor.add_node(node);
-
-    spinThread = std::thread([this]()
-    {
-      executor.spin();
-    });
 
     attachService =
       node->create_service<linkattacher_msgs::srv::AttachLink>(
@@ -72,12 +67,12 @@ public:
     RCLCPP_INFO(node->get_logger(),"LinkAttacher plugin ready");
   }
 
-///////////////////////////////////////////////////////////////
-
   void PreUpdate(
     const UpdateInfo &,
     EntityComponentManager &_ecm) override
   {
+
+    executor.spin_some();
 
     if (attachRequested)
     {
@@ -93,14 +88,11 @@ public:
 
   }
 
-///////////////////////////////////////////////////////////////
 
 private:
 
   rclcpp::Node::SharedPtr node;
-
   rclcpp::executors::SingleThreadedExecutor executor;
-  std::thread spinThread;
 
   rclcpp::Service<linkattacher_msgs::srv::AttachLink>::SharedPtr attachService;
   rclcpp::Service<linkattacher_msgs::srv::DetachLink>::SharedPtr detachService;
@@ -116,52 +108,87 @@ private:
   std::string model2;
   std::string link2;
 
-///////////////////////////////////////////////////////////////
+/*  FIND MODEL ENTITY  */
 
-  Entity FindLink(
+  Entity FindModel(
       EntityComponentManager &_ecm,
-      const std::string &linkName)
+      const std::string &modelName)
   {
 
-    Entity result{kNullEntity};
+    Entity modelEntity{kNullEntity};
 
     _ecm.Each<components::Name>(
       [&](const Entity &_entity,
           const components::Name *_name)
       {
 
-        if (_name->Data()==linkName)
+        if (_name->Data() == modelName)
         {
-          result=_entity;
+          modelEntity = _entity;
           return false;
         }
 
         return true;
+      });
 
+    return modelEntity;
+  }
+
+/*  FIND LINK INSIDE MODEL  */
+
+  Entity FindLink(
+      EntityComponentManager &_ecm,
+      const Entity &modelEntity,
+      const std::string &linkName)
+  {
+
+    Entity result{kNullEntity};
+
+    _ecm.Each<components::Name, components::ParentEntity>(
+      [&](const Entity &_entity,
+          const components::Name *_name,
+          const components::ParentEntity *_parent)
+      {
+
+        if (_name->Data() == linkName &&
+            _parent->Data() == modelEntity)
+        {
+          result = _entity;
+          return false;
+        }
+
+        return true;
       });
 
     return result;
   }
 
-///////////////////////////////////////////////////////////////
+/*  CREATE JOINT  */
 
   void CreateJoint(EntityComponentManager &_ecm)
   {
 
-    Entity parentLink = FindLink(_ecm, link1);
-    Entity childLink = FindLink(_ecm, link2);
+    Entity model1Entity = FindModel(_ecm, model1);
+    Entity model2Entity = FindModel(_ecm, model2);
+
+    if (model1Entity == kNullEntity || model2Entity == kNullEntity)
+    {
+      RCLCPP_ERROR(node->get_logger(),"Model not found");
+      return;
+    }
+
+    Entity parentLink = FindLink(_ecm, model1Entity, link1);
+    Entity childLink  = FindLink(_ecm, model2Entity, link2);
 
     if (parentLink == kNullEntity || childLink == kNullEntity)
     {
-      RCLCPP_ERROR(node->get_logger(),"Links not found");
+      RCLCPP_ERROR(node->get_logger(),"Link not found");
       return;
     }
 
     jointEntity = _ecm.CreateEntity();
 
-    _ecm.CreateComponent(
-      jointEntity,
-      components::Joint());
+    _ecm.CreateComponent(jointEntity, components::Joint());
 
     _ecm.CreateComponent(
       jointEntity,
@@ -173,13 +200,23 @@ private:
 
     _ecm.CreateComponent(
       jointEntity,
+      components::JointChild(childLink));
+
+    _ecm.CreateComponent(
+      jointEntity,
       components::JointType(sdf::JointType::FIXED));
 
-    RCLCPP_INFO(node->get_logger(),"Joint CREATED between objects");
+    RCLCPP_INFO(
+      node->get_logger(),
+      "Joint CREATED: %s::%s -> %s::%s",
+      model1.c_str(),
+      link1.c_str(),
+      model2.c_str(),
+      link2.c_str());
 
   }
 
-///////////////////////////////////////////////////////////////
+/*  REMOVE JOINT  */
 
   void RemoveJoint(EntityComponentManager &_ecm)
   {
@@ -195,7 +232,7 @@ private:
 
   }
 
-///////////////////////////////////////////////////////////////
+/*  SERVICE CALLBACKS  */
 
   void Attach(
     const std::shared_ptr<linkattacher_msgs::srv::AttachLink::Request> req,
@@ -203,19 +240,20 @@ private:
   {
 
     model1 = req->model1_name;
-    link1 = req->link1_name;
+    link1  = req->link1_name;
     model2 = req->model2_name;
-    link2 = req->link2_name;
+    link2  = req->link2_name;
 
     attachRequested = true;
 
-    RCLCPP_INFO(node->get_logger(),"Attach request received");
+    RCLCPP_INFO(
+      node->get_logger(),
+      "Attach request received");
 
     res->success = true;
     res->message = "Attach scheduled";
-  }
 
-///////////////////////////////////////////////////////////////
+  }
 
   void Detach(
     const std::shared_ptr<linkattacher_msgs::srv::DetachLink::Request>,
@@ -224,10 +262,13 @@ private:
 
     detachRequested = true;
 
-    RCLCPP_INFO(node->get_logger(),"Detach request received");
+    RCLCPP_INFO(
+      node->get_logger(),
+      "Detach request received");
 
     res->success = true;
     res->message = "Detach scheduled";
+
   }
 
 };
