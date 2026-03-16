0a93002b2198d4f10454ca0b88cb2b9e6391d285
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-03-16 10:15:17 +0100

    Format launchers

--- Launchers/warehouse2_ackermann_logistic_robot_harmonic.launch.py
+++ Launchers/warehouse2_ackermann_logistic_robot_harmonic.launch.py
@@ -9,13 +9,19 @@ as part of the world itself.
 
 Extends GZ_SIM_RESOURCE_PATH with custom_robots models.
 """
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, AppendEnvironmentVariable
+from launch.actions import (
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+    AppendEnvironmentVariable,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
 
@@ -25,12 +31,16 @@ def generate_launch_description():
     gazebo_models_path = os.path.join(package_dir, "models")
 
     robot_launch_dir = "/opt/jderobot/Launchers/ackermann_logistic_robot_harmonic"
-    gui_config_path = "/opt/jderobot/Launchers/visualization/amazon_robot_harmonic.config"
+    gui_config_path = (
+        "/opt/jderobot/Launchers/visualization/amazon_robot_harmonic.config"
+    )
 
     world_path = os.path.join("/opt/jderobot/Worlds", "warehouse2_ackerman.world")
 
     gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(ros_gz_sim, "launch", "gz_sim.launch.py")),
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
         launch_arguments={
             "gz_args": [f"-r -s -v4 --gui-config {gui_config_path} {world_path}"],
             "on_exit_shutdown": "true",
@@ -38,7 +48,9 @@ def generate_launch_description():
     )
 
     spawn_robot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(robot_launch_dir, "spawn_robot.launch.py"))
+        PythonLaunchDescriptionSource(
+            os.path.join(robot_launch_dir, "spawn_robot.launch.py")
+        )
     )
 
     ld = LaunchDescription()
