bdfecef863fea670f0b0a1f32c439317e731c9de
jepear19 <jesusper2010@gmail.com>
2026-03-19 20:13:44 +0100

    Fix path to world in launcher

--- Launchers/conveyor/conveyor_belt.launch.py
+++ Launchers/conveyor/conveyor_belt.launch.py
@@ -1,26 +1,14 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable, AppendEnvironmentVariable
-from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 import os
-
 
 def generate_launch_description():
 
-    # 🔹 Ruta al mundo
-    world_path = "/home/ws/Worlds/conveyor_world.world"
+    world_path = "/opt/jderobot/Worlds/conveyor_world.world"
 
-    # 🔹 IMPORTANTE: rutas de modelos
-    models_path = "/home/ws/CustomRobots"
-
-    # 🔹 (opcional pero recomendable)
-    gz_ros2_control_install = "/home/ws/install"
-    gz_lib_path = os.path.join(gz_ros2_control_install, "gz_ros2_control", "lib")
-
-    # 🔹 ENV correcto (esto es la clave)
     gz_env = {
-        "GZ_SIM_RESOURCE_PATH": models_path,
-        "GZ_SIM_SYSTEM_PLUGIN_PATH": gz_lib_path + ":/opt/ros/humble/lib",
-        "LD_LIBRARY_PATH": gz_lib_path + ":/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
+        "GZ_SIM_RESOURCE_PATH": "/opt/jderobot/CustomRobots/models:/opt/jderobot/Worlds",
+        "GZ_SIM_SYSTEM_PLUGIN_PATH": "/opt/ros/humble/lib",
         "DISPLAY": ":2",
     }
 
@@ -28,26 +16,12 @@ def generate_launch_description():
         cmd=["gz", "sim", "-s", "-r", "-v", "4", world_path],
         output="screen",
         additional_env=gz_env,
-        shell=False,
     )
 
-    ld = LaunchDescription()
-
-    # 🔥 MUY IMPORTANTE (esto te faltaba)
-    ld.add_action(
+    return LaunchDescription([
         SetEnvironmentVariable(
             "GZ_SIM_RESOURCE_PATH",
-            models_path
-        )
-    )
-
-    ld.add_action(
-        AppendEnvironmentVariable(
-            "GZ_SIM_RESOURCE_PATH",
-            models_path
-        )
-    )
-
-    ld.add_action(gazebo)
-
-    return ld
+            "/opt/jderobot/CustomRobots/models:/opt/jderobot/Worlds"
+        ),
+        gazebo
+    ])
