9196f0a3f82b6b2a8625758042f244db13b563c8
jepear19 <jesusper2010@gmail.com>
2026-03-19 22:19:34 +0100

    Fix model.sdf and launcher

--- Launchers/conveyor/conveyor_belt.launch.py
+++ Launchers/conveyor/conveyor_belt.launch.py
@@ -1,14 +1,31 @@
+import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
 
-    base_dir = os.path.dirname(__file__)
+    custom_pkg = get_package_share_directory("custom_robots")
+    ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
-    world_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(base_dir, "world.launch.py"))
+    world_path = "/opt/jderobot/Worlds/conveyor_world.world"
+
+    resource_path = custom_pkg + ":/usr/share/gz/gz-sim"
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={
+            "gz_args": ["-r -v4 ", world_path],
+            "on_exit_shutdown": "true",
+            "gz_sim_resource_path": resource_path,
+        }.items(),
     )
 
-    return LaunchDescription([world_launch])
+    ld = LaunchDescription()
+    ld.add_action(gazebo)
+
+    return ld
