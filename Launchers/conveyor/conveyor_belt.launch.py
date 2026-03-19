a0fe514e65e0e76053e0d1b23f1f11aa7c53daa2
jepear19 <jesusper2010@gmail.com>
2026-03-19 22:44:08 +0100

    Fix world and launch and delete world.launch.py

--- Launchers/conveyor/conveyor_belt.launch.py
+++ Launchers/conveyor/conveyor_belt.launch.py
@@ -7,25 +7,17 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    custom_pkg = get_package_share_directory("custom_robots")
     ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
     world_path = "/opt/jderobot/Worlds/conveyor_world.world"
-
-    resource_path = custom_pkg + ":/usr/share/gz/gz-sim"
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(ros_gz_sim, "launch", "gz_sim.launch.py")
         ),
         launch_arguments={
-            "gz_args": ["-r -v4 ", world_path],
-            "on_exit_shutdown": "true",
-            "gz_sim_resource_path": resource_path,
+            "gz_args": ["-r", "-v4", world_path],
         }.items(),
     )
 
-    ld = LaunchDescription()
-    ld.add_action(gazebo)
-
-    return ld
+    return LaunchDescription([gazebo])
