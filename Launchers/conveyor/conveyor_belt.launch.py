3255fd963eaa24fb7033b3648d4653e522520d4e
jepear19 <jesusper2010@gmail.com>
2026-03-19 20:53:34 +0100

    Fix in .world and .launch.py

--- Launchers/conveyor/conveyor_belt.launch.py
+++ Launchers/conveyor/conveyor_belt.launch.py
@@ -1,14 +1,22 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
 
+    custom_pkg = get_package_share_directory("custom_robots")
+
+    models_path = os.path.join(custom_pkg, "models")
+
     world_path = "/opt/jderobot/Worlds/conveyor_world.world"
 
+    resource_path = models_path + ":/opt/jderobot/Worlds"
+
     gz_env = {
-        "GZ_SIM_RESOURCE_PATH": "/opt/jderobot/CustomRobots/models:/opt/jderobot/Worlds",
+        "GZ_SIM_RESOURCE_PATH": models_path + ":/opt/jderobot/Worlds",
         "GZ_SIM_SYSTEM_PLUGIN_PATH": "/opt/ros/humble/lib",
+        "LD_LIBRARY_PATH": "/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
         "DISPLAY": ":2",
     }
 
@@ -16,12 +24,7 @@ def generate_launch_description():
         cmd=["gz", "sim", "-s", "-r", "-v", "4", world_path],
         output="screen",
         additional_env=gz_env,
+        shell=False,
     )
 
-    return LaunchDescription([
-        SetEnvironmentVariable(
-            "GZ_SIM_RESOURCE_PATH",
-            "/opt/jderobot/CustomRobots/models:/opt/jderobot/Worlds"
-        ),
-        gazebo
-    ])
+    return LaunchDescription([gazebo])
