ca3602e12e8c4874f5981cd0ce902ece6820ee9a
jepear19 <jesusper2010@gmail.com>
2026-03-19 21:33:20 +0100

    Fix launcher y world

--- Launchers/conveyor/world.launch.py
+++ Launchers/conveyor/world.launch.py
@@ -1,24 +1,32 @@
 import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable, AppendEnvironmentVariable
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+
     custom_pkg = get_package_share_directory("custom_robots")
-    models_path = os.path.join(custom_pkg, "models")
+
+    models_path = custom_pkg
 
     world_path = "/opt/jderobot/Worlds/conveyor_world.world"
 
+    plugin_path = os.path.join(
+        os.path.expanduser("~"),
+        "ws/install/conveyor_belt_plugin/lib"
+    )
+
     gz_env = {
         "GZ_SIM_RESOURCE_PATH": models_path,
-        "GZ_SIM_SYSTEM_PLUGIN_PATH": "/opt/ros/humble/lib",
-        "LD_LIBRARY_PATH": "/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
-        "DISPLAY": ":2",
+        "GZ_SIM_SYSTEM_PLUGIN_PATH": plugin_path + ":/opt/ros/humble/lib",
+        "LD_LIBRARY_PATH": os.environ.get("LD_LIBRARY_PATH", "")
+        + ":" + plugin_path + ":/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
+        "DISPLAY": os.environ.get("DISPLAY", ":0"),
     }
 
     gazebo = ExecuteProcess(
-        cmd=["gz", "sim", "-s", "-r", "-v", "4", world_path],
+        cmd=["gz", "sim", "-r", "-v", "4", world_path],
         output="screen",
         additional_env=gz_env,
         shell=False,
@@ -28,13 +36,6 @@ def generate_launch_description():
 
     ld.add_action(
         SetEnvironmentVariable(
-            "GZ_SIM_RESOURCE_PATH",
-            models_path
-        )
-    )
-
-    ld.add_action(
-        AppendEnvironmentVariable(
             "GZ_SIM_RESOURCE_PATH",
             models_path
         )
