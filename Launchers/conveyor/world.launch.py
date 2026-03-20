51d98880884191fbc62c178105347c69f5c3dcf0
jepear19 <jesusper2010@gmail.com>
2026-03-20 10:08:51 +0100

    Fix world launcher

--- Launchers/conveyor/world.launch.py
+++ Launchers/conveyor/world.launch.py
@@ -1,20 +1,33 @@
 import os
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
     world_path = "/opt/jderobot/Worlds/mi_mundo.world"
 
-    set_display = SetEnvironmentVariable("DISPLAY", ":1")
+    gz_env = {
+        "DISPLAY": ":2",
+    }
 
     gazebo = ExecuteProcess(
-        cmd=["gz", "sim", world_path],
-        output="screen"
+        cmd=["gz", "sim", "-s", "-r", "-v", "4", world_path],
+        output="screen",
+        additional_env=gz_env,
+        shell=False,
+    )
+
+    world_entity_cmd = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-name", "world", "-file", world_path],
+        output="screen",
     )
 
     return LaunchDescription([
-        set_display,
-        gazebo
+        SetEnvironmentVariable("DISPLAY", ":2"),
+        gazebo,
+        world_entity_cmd,
     ])
