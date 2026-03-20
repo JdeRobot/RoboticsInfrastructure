8c7c28543201394b51706198df2bab4ee8f876bc
jepear19 <jesusper2010@gmail.com>
2026-03-20 11:31:38 +0100

    Update branch

--- Launchers/conveyor/world.launch.py
+++ Launchers/conveyor/world.launch.py
@@ -9,10 +9,13 @@ def generate_launch_description():
     world_path = "/opt/jderobot/Worlds/conveyor_world.world"
 
     resource_path = "/home/ws/src/CustomRobots:/opt/jderobot/Worlds"
+    gz_lib_path = "/home/ws/install"
 
     gz_env = {
         "DISPLAY": ":2",
         "GZ_SIM_RESOURCE_PATH": resource_path,
+        "GZ_SIM_SYSTEM_PLUGIN_PATH": gz_lib_path + ":/opt/ros/humble/lib",
+        "LD_LIBRARY_PATH": gz_lib_path + ":/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
     }
 
     gazebo = ExecuteProcess(
@@ -31,7 +34,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         SetEnvironmentVariable("DISPLAY", ":2"),
-        SetEnvironmentVariable("GZ_SIM_RESOURCE_PATH", resource_path),  # 🔴 CLAVE
+        SetEnvironmentVariable("GZ_SIM_RESOURCE_PATH", resource_path),
+        SetEnvironmentVariable("GZ_SIM_SYSTEM_PLUGIN_PATH", gz_lib_path + ":/opt/ros/humble/lib"),
+        SetEnvironmentVariable("LD_LIBRARY_PATH", gz_lib_path + ":/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu"),
         gazebo,
         world_entity_cmd,
     ])
