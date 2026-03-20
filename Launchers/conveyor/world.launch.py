498f0384fac2604798212a4ec9788a9885723a16
jepear19 <jesusper2010@gmail.com>
2026-03-20 09:46:48 +0100

    Fix Launcher

--- Launchers/conveyor/world.launch.py
+++ Launchers/conveyor/world.launch.py
@@ -1,28 +1,20 @@
 import os
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
-    package_dir = get_package_share_directory("custom_robots")
+    world_path = "/opt/jderobot/Worlds/mi_mundo.world"
 
-    world_path = os.path.join(package_dir, "worlds", "mi_mundo.world")
-
-    gazebo_models_path = os.path.join(package_dir, "models")
-
-    set_env = SetEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH",
-        gazebo_models_path
-    )
+    set_display = SetEnvironmentVariable("DISPLAY", ":1")
 
     gazebo = ExecuteProcess(
-        cmd=["gz", "sim", "-r", "-v", "4", world_path],
+        cmd=["gz", "sim", world_path],
         output="screen"
     )
 
     return LaunchDescription([
-        set_env,
+        set_display,
         gazebo
     ])
