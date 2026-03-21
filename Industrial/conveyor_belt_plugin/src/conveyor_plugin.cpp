fe24814886e2ac4f15b6ddc82120adb0b4245df7
jepear19 <jesusper2010@gmail.com>
2026-03-21 18:28:29 +0100

    Fix plugin and collision

--- Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
+++ Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
@@ -1,25 +1,60 @@
 #include <gz/sim/System.hh>
+#include <gz/sim/Model.hh>
+#include <gz/sim/Link.hh>
+#include <gz/sim/Util.hh>
 #include <gz/plugin/Register.hh>
-#include <iostream>
 
 namespace conveyor
 {
 
 class ConveyorPlugin :
   public gz::sim::System,
-  public gz::sim::ISystemConfigure
+  public gz::sim::ISystemConfigure,
+  public gz::sim::ISystemPreUpdate
 {
 public:
 
+  gz::sim::Model model{gz::sim::kNullEntity};
+  gz::sim::Entity linkEntity{gz::sim::kNullEntity};
+  double velocity{0.2};
+
   void Configure(
-    const gz::sim::Entity &,
-    const std::shared_ptr<const sdf::Element> &,
-    gz::sim::EntityComponentManager &,
+    const gz::sim::Entity &entity,
+    const std::shared_ptr<const sdf::Element> &sdf,
+    gz::sim::EntityComponentManager &ecm,
     gz::sim::EventManager &) override
   {
-    std::cout << "[ConveyorPlugin] Loaded correctly" << std::endl;
+    this->model = gz::sim::Model(entity);
+
+    std::string linkName = sdf->Get<std::string>("link_name");
+    this->velocity = sdf->Get<double>("velocity");
+
+    this->linkEntity = this->model.LinkByName(ecm, linkName);
+
+    std::cout << "[ConveyorPlugin] Loaded, link: " << linkName << std::endl;
   }
 
+  void PreUpdate(
+    const gz::sim::UpdateInfo &,
+    gz::sim::EntityComponentManager &ecm) override
+  {
+    if (this->linkEntity == gz::sim::kNullEntity)
+      return;
+
+    auto velComp =
+      ecm.Component<gz::sim::components::LinearVelocity>(this->linkEntity);
+
+    if (!velComp)
+    {
+      ecm.CreateComponent(
+        this->linkEntity,
+        gz::sim::components::LinearVelocity({0, this->velocity, 0}));
+    }
+    else
+    {
+      velComp->Data() = {0, this->velocity, 0};
+    }
+  }
 };
 
 }
@@ -27,5 +62,6 @@ public:
 GZ_ADD_PLUGIN(
   conveyor::ConveyorPlugin,
   gz::sim::System,
-  gz::sim::ISystemConfigure
+  gz::sim::ISystemConfigure,
+  gz::sim::ISystemPreUpdate
 )
