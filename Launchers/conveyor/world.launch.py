6f2e1f66e04a1717a7b35d247d20170ae38e1893
jepear19 <jesusper2010@gmail.com>
2026-03-22 22:21:27 +0100

    Change launcher

--- Launchers/conveyor/world.launch.py
+++ Launchers/conveyor/world.launch.py
@@ -1,36 +1,26 @@
 import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
 
     world_path = "/opt/jderobot/Worlds/conveyor_world.world"
 
+    gz_env = {
+        "DISPLAY": ":2",
+        "GZ_SIM_RESOURCE_PATH": "/home/ws/src/CustomRobots",
+        "GZ_SIM_SYSTEM_PLUGIN_PATH": "/home/ws/install/conveyor_belt_plugin/lib:/opt/ros/humble/lib",
+        "LD_LIBRARY_PATH": "/home/ws/install/conveyor_belt_plugin/lib:/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
+    }
+
+    gazebo = ExecuteProcess(
+        cmd=["gz", "sim", "-r", "-v", "4", world_path],
+        output="screen",
+        additional_env=gz_env,
+        shell=False,
+    )
+
     return LaunchDescription([
-
-        SetEnvironmentVariable(
-            "GZ_SIM_RESOURCE_PATH",
-            "/home/ws/src/CustomRobots"
-        ),
-
-        SetEnvironmentVariable(
-            "GZ_SIM_SYSTEM_PLUGIN_PATH",
-            "/home/ws/install:/opt/ros/humble/lib"
-        ),
-
-        SetEnvironmentVariable(
-            "LD_LIBRARY_PATH",
-            "/home/ws/install:/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu"
-        ),
-
-        SetEnvironmentVariable(
-            "DISPLAY",
-            ":2"
-        ),
-
-        ExecuteProcess(
-            cmd=["gz", "sim", "-r", "-v", "4", world_path],
-            output="screen",
-        ),
+        gazebo,
     ])
