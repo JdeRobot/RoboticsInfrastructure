6d30123f814472de7d2c199c56293e9762eaccb8
Jose Miguel Jimenez Garcia <jm.jimenez.2020@alumnos.urjc.es>
2026-03-23 15:49:23 +0100

    plugin topics v7

--- CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
+++ CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
@@ -88,11 +88,11 @@ public:
 
         double yaw = pose.Rot().Yaw();
 
-        // Actualizar posición lineal
-        pose.Pos().X() += -lin * std::sin(yaw);
-        pose.Pos().Y() += -lin * std::cos(yaw);
+        // Mover hacia adelante según yaw actual
+        pose.Pos().X() += lin * std::cos(yaw);
+        pose.Pos().Y() += lin * std::sin(yaw);
 
-        // Actualizar rotación
+        // Girar según velocidad angular
         pose.Rot() = gz::math::Quaterniond(0, 0, yaw + ang);
 
         this->model.SetWorldPoseCmd(_ecm, pose);
