a03a077a53f9b92575d893669f6ee7f06eedca47
Jose Miguel Jimenez Garcia <jm.jimenez.2020@alumnos.urjc.es>
2026-03-23 12:22:00 +0100

    simple plugin v10

--- CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
+++ CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
@@ -1,73 +1,77 @@
 #include <gz/sim/System.hh>
 #include <gz/sim/Model.hh>
-#include <gz/sim/Util.hh>
 #include <gz/plugin/Register.hh>
 #include <gz/transport/Node.hh>
-#include <gz/math/Pose3.hh>
 #include <gz/msgs/twist.pb.h>
+#include <gz/sim/components/Pose.hh>
 
 #include <iostream>
 
 namespace person_plugin
 {
-  class Person:
+class Person :
     public gz::sim::System,
     public gz::sim::ISystemConfigure,
     public gz::sim::ISystemPreUpdate
-  {
-    private:
-      gz::sim::Model model{gz::sim::kNullEntity};
-      gz::transport::Node node;
-      double linear_speed{0.0}; // velocidad recibida del topic
+{
+private:
+    gz::sim::Model model{gz::sim::kNullEntity};
+    gz::transport::Node node;
+    double linear_speed{0.0};
 
-    public:
-      void Configure(const gz::sim::Entity &_entity,
-                     const std::shared_ptr<const sdf::Element> &,
-                     gz::sim::EntityComponentManager &_ecm,
-                     gz::sim::EventManager &) override
-      {
+public:
+    void Configure(const gz::sim::Entity &_entity,
+                   const std::shared_ptr<const sdf::Element> &,
+                   gz::sim::EntityComponentManager &_ecm,
+                   gz::sim::EventManager &) override
+    {
         this->model = gz::sim::Model(_entity);
-
         if (!this->model.Valid(_ecm))
         {
-          std::cerr << "[Person] Invalid model\n";
-          return;
+            std::cerr << "[Person] Invalid model\n";
+            return;
         }
 
-        // Suscribirse al topic /cmd_vel
+        // Suscribirse al topic /person/cmd_vel
         this->node.Subscribe("/person/cmd_vel", &Person::OnCmdVel, this);
 
         std::cout << "[Person] Plugin loaded\n";
-      }
+    }
 
-      // Callback que guarda la velocidad lineal
-      void OnCmdVel(const gz::msgs::Twist &_msg)
-      {
+    void OnCmdVel(const gz::msgs::Twist &_msg)
+    {
         this->linear_speed = _msg.linear().x();
-      }
+    }
 
-      void PreUpdate(const gz::sim::UpdateInfo &_info,
-                     gz::sim::EntityComponentManager &_ecm) override
-      {
+    void PreUpdate(const gz::sim::UpdateInfo &_info,
+                   gz::sim::EntityComponentManager &_ecm) override
+    {
         if (_info.paused || !this->model.Valid(_ecm))
-          return;
+            return;
 
-        // Leer pose actual del mundo
-        auto pose = this->model.WorldPose(_ecm);
+        // Obtener la pose actual desde ECM
+        auto poseComp = _ecm.Component<gz::sim::components::Pose>(this->model.Entity());
+        if (!poseComp)
+            return;
+
+        auto pose = poseComp->Data();
         double yaw = pose.Rot().Yaw();
+
+        // Actualizar posición
         pose.Pos().X() += this->linear_speed * std::cos(yaw);
         pose.Pos().Y() += this->linear_speed * std::sin(yaw);
+
+        // Aplicar nueva pose
         this->model.SetWorldPoseCmd(_ecm, pose);
-      }
-  };
+    }
+};
 }
 
-// Registrar el plugin
+// Registrar plugin
 GZ_ADD_PLUGIN(
-  person_plugin::Person,
-  gz::sim::System,
-  person_plugin::Person::ISystemConfigure,
-  person_plugin::Person::ISystemPreUpdate
+    person_plugin::Person,
+    gz::sim::System,
+    person_plugin::Person::ISystemConfigure,
+    person_plugin::Person::ISystemPreUpdate
 )
-
 GZ_ADD_PLUGIN_ALIAS(person_plugin::Person, "person_plugin::Person")
