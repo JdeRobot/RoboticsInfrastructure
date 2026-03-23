d043741a04c90159d50ccfa35117b11686190df1
Jose Miguel Jimenez Garcia <jm.jimenez.2020@alumnos.urjc.es>
2026-03-23 10:46:59 +0100

    simple plugin v3

--- CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
+++ CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
@@ -4,63 +4,101 @@
 #include <gz/plugin/Register.hh>
 
 #include <gz/math/Pose3.hh>
+#include <gz/math/Quaternion.hh>
 
-#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <memory>
+#include <mutex>
 
 namespace person_plugin
 {
-  class Person:
+class Person :
     public gz::sim::System,
     public gz::sim::ISystemConfigure,
     public gz::sim::ISystemPreUpdate
-  {
-    private:
-      gz::sim::Model model{gz::sim::kNullEntity};
-      double linear_speed{0.001};  // MUY pequeño para probar
+{
+private:
+    gz::sim::Model model{gz::sim::kNullEntity};
+    std::mutex mtx_;
 
-    public:
-      void Configure(const gz::sim::Entity &_entity,
-                     const std::shared_ptr<const sdf::Element> &,
-                     gz::sim::EntityComponentManager &_ecm,
-                     gz::sim::EventManager &) override
-      {
+    // Velocidades recibidas desde el topic
+    double linear_x{0.0};
+    double angular_z{0.0};
+
+    std::shared_ptr<rclcpp::Node> node_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_;
+
+public:
+    void Configure(const gz::sim::Entity &_entity,
+                   const std::shared_ptr<const sdf::Element> &,
+                   gz::sim::EntityComponentManager &_ecm,
+                   gz::sim::EventManager &) override
+    {
         this->model = gz::sim::Model(_entity);
-
         if (!this->model.Valid(_ecm))
         {
-          std::cerr << "[Person] Invalid model\n";
-          return;
+            std::cerr << "[Person] Invalid model\n";
+            return;
         }
 
         std::cout << "[Person] Plugin loaded\n";
-      }
 
-      void PreUpdate(const gz::sim::UpdateInfo &_info,
-                     gz::sim::EntityComponentManager &_ecm) override
-      {
+        // Inicializar ROS 2 si no lo está
+        if (!rclcpp::ok())
+            rclcpp::init(0, nullptr);
+
+        node_ = std::make_shared<rclcpp::Node>("person_plugin_node");
+
+        // Suscribirse al topic de velocidades
+        sub_ = node_->create_subscription<geometry_msgs::msg::Twist>(
+            "/person/cmd_vel", 10,
+            [this](const geometry_msgs::msg::Twist::SharedPtr msg)
+            {
+                std::lock_guard<std::mutex> lock(this->mtx_);
+                this->linear_x = msg->linear.x;
+                this->angular_z = msg->angular.z;
+            });
+    }
+
+    void PreUpdate(const gz::sim::UpdateInfo &_info,
+                   gz::sim::EntityComponentManager &_ecm) override
+    {
         if (_info.paused || !this->model.Valid(_ecm))
-          return;
+            return;
 
-        // 🔑 Leer SIEMPRE la pose actual del mundo
+        // Procesar callbacks de ROS 2
+        rclcpp::spin_some(node_);
+
+        // Obtener pose actual
         auto pose = gz::sim::worldPose(this->model.Entity(), _ecm);
-
         double yaw = pose.Rot().Yaw();
 
-        // Movimiento forward muy pequeño
-        pose.Pos().X() += this->linear_speed * std::cos(yaw);
-        pose.Pos().Y() += this->linear_speed * std::sin(yaw);
+        double lin, ang;
+        {
+            std::lock_guard<std::mutex> lock(this->mtx_);
+            lin = this->linear_x;
+            ang = this->angular_z;
+        }
 
-        // Aplicar movimiento
+        // Aplicar giro
+        pose.Rot() = gz::math::Quaterniond(0, 0, yaw + ang);
+
+        // Movimiento hacia delante según yaw
+        pose.Pos().X() += lin * -std::sin(yaw);
+        pose.Pos().Y() += lin *  std::cos(yaw);
+
+        // Aplicar la nueva pose
         this->model.SetWorldPoseCmd(_ecm, pose);
-      }
-  };
+    }
+};
 }
 
 GZ_ADD_PLUGIN(
-  person_plugin::Person,
-  gz::sim::System,
-  person_plugin::Person::ISystemConfigure,
-  person_plugin::Person::ISystemPreUpdate
+    person_plugin::Person,
+    gz::sim::System,
+    person_plugin::Person::ISystemConfigure,
+    person_plugin::Person::ISystemPreUpdate
 )
 
 GZ_ADD_PLUGIN_ALIAS(person_plugin::Person, "person_plugin::Person")
