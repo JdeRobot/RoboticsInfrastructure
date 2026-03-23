3fbe9a4698224a3fc7f57de64cc86dbaf08a66f4
Jose Miguel Jimenez Garcia <jm.jimenez.2020@alumnos.urjc.es>
2026-03-23 10:27:29 +0100

    simple plugin v2

--- CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
+++ CustomRobots/follow_person_harmonic/src/person_harmonic.cpp
@@ -1,6 +1,9 @@
 #include <gz/sim/System.hh>
 #include <gz/sim/Model.hh>
+#include <gz/sim/Util.hh>
 #include <gz/plugin/Register.hh>
+
+#include <gz/math/Pose3.hh>
 
 #include <iostream>
 
@@ -8,10 +11,12 @@ namespace person_plugin
 {
   class Person:
     public gz::sim::System,
-    public gz::sim::ISystemConfigure
+    public gz::sim::ISystemConfigure,
+    public gz::sim::ISystemPreUpdate
   {
     private:
       gz::sim::Model model{gz::sim::kNullEntity};
+      double linear_speed{0.001};  // MUY pequeño para probar
 
     public:
       void Configure(const gz::sim::Entity &_entity,
@@ -27,7 +32,26 @@ namespace person_plugin
           return;
         }
 
-        std::cout << "[Person] Plugin loaded correctly\n";
+        std::cout << "[Person] Plugin loaded\n";
+      }
+
+      void PreUpdate(const gz::sim::UpdateInfo &_info,
+                     gz::sim::EntityComponentManager &_ecm) override
+      {
+        if (_info.paused || !this->model.Valid(_ecm))
+          return;
+
+        // 🔑 Leer SIEMPRE la pose actual del mundo
+        auto pose = gz::sim::worldPose(this->model.Entity(), _ecm);
+
+        double yaw = pose.Rot().Yaw();
+
+        // Movimiento forward muy pequeño
+        pose.Pos().X() += this->linear_speed * std::cos(yaw);
+        pose.Pos().Y() += this->linear_speed * std::sin(yaw);
+
+        // Aplicar movimiento
+        this->model.SetWorldPoseCmd(_ecm, pose);
       }
   };
 }
@@ -35,7 +59,8 @@ namespace person_plugin
 GZ_ADD_PLUGIN(
   person_plugin::Person,
   gz::sim::System,
-  person_plugin::Person::ISystemConfigure
+  person_plugin::Person::ISystemConfigure,
+  person_plugin::Person::ISystemPreUpdate
 )
 
 GZ_ADD_PLUGIN_ALIAS(person_plugin::Person, "person_plugin::Person")
