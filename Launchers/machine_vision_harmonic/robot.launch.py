b60ca3aeb4aea5ba44c4bf8ab39b049bffa76d64
jepear19 <jesusper2010@gmail.com>
2026-03-24 21:05:04 +0100

    Restart robot.launch.py

--- Launchers/machine_vision_harmonic/robot.launch.py
+++ Launchers/machine_vision_harmonic/robot.launch.py
@@ -1,303 +1,80 @@
-#!/usr/bin/python3
+"""
+Machine Vision Harmonic - Robot Launcher (FIXED)
+"""
 
-# ===================================== COPYRIGHT ===================================== #
-#                                                                                       #
-#  IFRA (Intelligent Flexible Robotics and Assembly) Group, CRANFIELD UNIVERSITY        #
-#  Created on behalf of the IFRA Group at Cranfield University, United Kingdom          #
-#  E-mail: IFRA@cranfield.ac.uk                                                         #
-#                                                                                       #
-#  Licensed under the Apache-2.0 License.                                               #
-#  You may not use this file except in compliance with the License.                     #
-#  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0  #
-#                                                                                       #
-#  Unless required by applicable law or agreed to in writing, software distributed      #
-#  under the License is distributed on an "as-is" basis, without warranties or          #
-#  conditions of any kind, either express or implied. See the License for the specific  #
-#  language governing permissions and limitations under the License.                    #
-#                                                                                       #
-#  IFRA Group - Cranfield University                                                    #
-#  AUTHORS: Mikel Bueno Viso - Mikel.Bueno-Viso@cranfield.ac.uk                         #
-#           Dr. Seemal Asif  - s.asif@cranfield.ac.uk                                   #
-#           Prof. Phil Webb  - p.f.webb@cranfield.ac.uk                                 #
-#                                                                                       #
-#  Date: June, 2024.                                                                    #
-#                                                                                       #
-# ===================================== COPYRIGHT ===================================== #
-
-# ======= CITE OUR WORK ======= #
-# You can cite our work with the following statement:
-# IFRA-Cranfield (2023) ROS 2 Sim-to-Real Robot Control. URL: https://github.com/IFRA-Cranfield/ros2_SimRealRobotControl.
-
-# simulation.launch.py:
-# Launch file for the ROBOT's GAZEBO SIMULATION in ROS2 Humble:
-
-# Import libraries:
-import os, sys, xacro, yaml
-from ament_index_python.packages import get_package_share_directory, PackageNotFoundError
+import os
+import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
-# LOAD FILE:
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError:
-        # parent of IOError, OSError *and* WindowsError where available.
-        return None
-# LOAD YAML:
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:
-        # parent of IOError, OSError *and* WindowsError where available.
-        return None
 
-# ===== REQUIRED TO GET THE ROBOT CONFIGURATION === #
-
-# EVALUATE INPUT ARGUMENTS:
-def AssignArgument(ARGUMENT):
-    ARGUMENTS = sys.argv
-    for y in ARGUMENTS:
-        if (ARGUMENT + ":=") in y:
-            ARG = y.replace((ARGUMENT + ":="),"")
-            return(ARG)
-
-# GET CONFIGURATION from YAML:
-def GetCONFIG(CONFIGURATION, PKG_PATH):
-    
-    RESULT = {"Success": False, "ID": "", "Name": "", "urdf": "", "ee": ""}
-    
-    YAML_PATH = PKG_PATH + "/config/configurations.yaml"
-    
-    if not os.path.exists(YAML_PATH):
-        return (RESULT)
-    
-    with open(YAML_PATH, 'r') as YAML:
-        cYAML = yaml.safe_load(YAML)
-
-    for x in cYAML["Configurations"]:
-
-        if x["ID"] == CONFIGURATION:
-            RESULT["Success"] = True
-            RESULT["ID"] = x["ID"]
-            RESULT["Name"] = x["Name"]
-            RESULT["urdf"] = x["urdf"]
-            RESULT["rob"] = x["rob"]
-            RESULT["ee"] = x["ee"]
-
-    return(RESULT)
-
-# GET EE-Controllers LIST:
-def GetEEctr(EEName):
-    
-    RESULT = []
-
-    PATH = os.path.join(os.path.expanduser('~'), 'dev_ws', 'src', 'IndustrialRobots', 'ros2_SimRealRobotControl', 'ros2srrc_endeffectors', EEName, 'config')
-    YAML_PATH = PATH + "/controller_moveit2.yaml"
-    
-    with open(YAML_PATH, 'r') as YAML:
-        cYAML = yaml.safe_load(YAML)
-
-    for x in cYAML["controller_names"]:
-        RESULT.append(x)
-
-    return(RESULT)
-
-# CHECK if CONTROLLER file exists for EE:
-def EEctrlEXISTS(EEName):
-    
-    PATH = os.path.join(os.path.expanduser('~'), 'dev_ws', 'src', 'IndustrialRobots', 'ros2_SimRealRobotControl', 'ros2srrc_endeffectors', EEName, 'config')
-    YAML_PATH = PATH + "/controller.yaml"
-    
-    RES = os.path.exists(YAML_PATH)
-    return(RES)
-
-# ========== **GENERATE LAUNCH DESCRIPTION** ========== #
 def generate_launch_description():
-    
-    LD = LaunchDescription()
-    
-    # === INPUT ARGUMENT: ROS 2 PACKAGE === #
-    PACKAGE_NAME = AssignArgument("package")
-    if PACKAGE_NAME != None:
-        None
-    else:
-        print("")
-        print("ERROR: package INPUT ARGUMENT has not been defined. Please try again.")
-        print("Closing... BYE!")
-        exit()
-        
-    # CHECK if -> PACKAGE EXISTS, and GET PATH:
-    try:
-        PKG_PATH = get_package_share_directory(PACKAGE_NAME + "_gazebo")
-    except PackageNotFoundError:
-        print("")
-        print("ERROR: The defined ROS 2 Package was not found. Please try again.")
-        print("Closing... BYE!")
-        exit()
-    except ValueError:
-        print("")
-        print("ERROR: The defined ROS 2 Package name is not valid. Please try again.")
-        print("Closing... BYE!")
-        exit()
-    
-    # === INPUT ARGUMENT: CONFIGURATION === #
-    CONFIG = AssignArgument("config")
-    CONFIGURATION = GetCONFIG(CONFIG, PKG_PATH)
 
-    if CONFIGURATION["Success"] == False:
-        print("")
-        print("ERROR: config INPUT ARGUMENT has not been correctly defined. Please try again.")
-        print("Closing... BYE!")
-        exit()   
+    PACKAGE_NAME = "ros2srrc_ur5"
+    pkg_path = get_package_share_directory(PACKAGE_NAME + "_gazebo")
 
-    # === INPUT ARGUMENT: HMI === #
-    HMI = AssignArgument("hmi")
-    if HMI == "True" or HMI == "true":
-        HMI = "true"
-    else:
-        HMI = "false"
+    xacro_file = os.path.join(pkg_path, "urdf", "ur5.urdf.xacro")
 
-    # ========== CELL INFORMATION ========== #
-    print("")
-    print("===== GAZEBO: Robot Simulation (" + PACKAGE_NAME + "_gazebo) =====")
-    print("Robot configuration:")
-    print(CONFIGURATION["ID"] + " -> " + CONFIGURATION["Name"])
-    print("")
-    
-    # ***** GAZEBO ***** #   
-    # DECLARE Gazebo WORLD file:
-    robot_gazebo = os.path.join(
-        get_package_share_directory(PACKAGE_NAME + '_gazebo'),
-        'worlds',
-        PACKAGE_NAME + '_machie_vision'+'.world')
-    # DECLARE Gazebo LAUNCH file:
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-                launch_arguments={'world': robot_gazebo}.items(),
-            )
-
-    # ***** ROBOT DESCRIPTION ***** #
-    # Robot Description file package:
-    robot_description_path = os.path.join(get_package_share_directory(PACKAGE_NAME + '_gazebo'))
-    # ROBOT urdf file path:
-    xacro_file = os.path.join(robot_description_path,'urdf',CONFIGURATION["urdf"])
-    # Generate ROBOT_DESCRIPTION variable:
     doc = xacro.parse(open(xacro_file))
-    
-    if CONFIGURATION["ee"] == "none":
-        EE = "false"
-    else:
-        EE = "true"
-    
-    xacro.process_doc(doc, mappings={
-        "EE": EE,
-        "EE_name": CONFIGURATION["ee"],
-        "hmi": HMI,
-    })
-    
-    # EE -> Controller file needed?
-    if EE == "true":
-        if EEctrlEXISTS(CONFIGURATION["ee"]) == False:
-            EE = "true-NOctr"
-    
-    robot_description_config = doc.toxml()
-    robot_description = {'robot_description': robot_description_config}
+    xacro.process_doc(doc)
 
-    # ROBOT STATE PUBLISHER NODE:
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='both',
-        parameters=[
-            robot_description,
-            {"use_sim_time": True}
-        ]
+    robot_description = {"robot_description": doc.toxml()}
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description, {"use_sim_time": True}],
     )
 
-    # SPAWN ROBOT TO GAZEBO:
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description','-entity', CONFIGURATION["rob"]],
-                        output='both')
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0", "0", "0", "0", "0", "0", "world", "base_link"],
+    )
 
-    # ***** CONTROLLERS ***** #
-    # Joint STATE BROADCASTER:
-    joint_state_broadcaster_spawner = Node(
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "ur5",
+            "-topic", "robot_description",
+            "-x", "0",
+            "-y", "0",
+            "-z", "0",
+            "-R", "0",
+            "-P", "0",
+            "-Y", "0",
+        ],
+        output="screen",
+    )
+
+    # Controllers
+    joint_state_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        arguments=["joint_state_broadcaster"],
     )
-    # Joint TRAJECTORY Controller:
-    joint_trajectory_controller_spawner = Node(
+
+    joint_trajectory_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+        arguments=["joint_trajectory_controller"],
     )
 
-    # EE CONTROLLERS:
-    if EE == "true":
-        CONTROLLERS = GetEEctr(CONFIGURATION["ee"])
-        CONTROLLER_NODES = []
-
-        for x in CONTROLLERS:
-            CONTROLLER_NODES.append(
-                Node(
-                    package="controller_manager",
-                    executable="spawner",
-                    arguments=[x, "-c", "/controller_manager"],
-                )
-            )
-
-    # =============================================== #
-    # ========== RETURN LAUNCH DESCRIPTION ========== #
-
-    # Add ROS 2 Nodes to LaunchDescription() element:
-    LD.add_action(gazebo)
-    LD.add_action(node_robot_state_publisher)
-    LD.add_action(spawn_entity)
-
-    LD.add_action(RegisterEventHandler(
+    delay_controllers = RegisterEventHandler(
         OnProcessExit(
-            target_action = spawn_entity,
-            on_exit = [
-                joint_state_broadcaster_spawner,
-                ]
-            )
+            target_action=spawn_entity,
+            on_exit=[joint_state_broadcaster, joint_trajectory_controller],
         )
     )
 
-    LD.add_action(RegisterEventHandler(
-        OnProcessExit(
-            target_action = spawn_entity,
-            on_exit = [
-                joint_trajectory_controller_spawner,
-                ]
-            )
-        )
-    )
-
-    if EE == "true":
-
-        for x in CONTROLLER_NODES:
-
-            LD.add_action(RegisterEventHandler(
-                OnProcessExit(
-                    target_action = joint_trajectory_controller_spawner,
-                    on_exit = [
-                        x,
-                        ]
-                    )
-                )
-            )
-
-    # ***** RETURN  ***** #
-    return(LD)
+    return LaunchDescription([
+        robot_state_publisher,
+        static_tf,
+        spawn_entity,
+        delay_controllers,
+    ])
