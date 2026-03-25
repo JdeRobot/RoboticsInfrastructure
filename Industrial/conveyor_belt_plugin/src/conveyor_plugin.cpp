62569a40fb54d457a05d40b5050efedc1d4f7994
jepear19 <jesusper2010@gmail.com>
2026-03-25 10:54:19 +0100

    Fix plugin and model.sdf

--- Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
+++ Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
@@ -10,41 +10,40 @@ namespace conveyor
 {
 
 class ConveyorPlugin :
-  public gz::sim::System,
-  public gz::sim::ISystemConfigure,
-  public gz::sim::ISystemPreUpdate
+  public ::gz::sim::System,
+  public ::gz::sim::ISystemConfigure,
+  public ::gz::sim::ISystemPreUpdate
 {
 public:
 
-  gz::sim::Entity beltLink{gz::sim::kNullEntity};
+  ::gz::sim::Entity beltLink{::gz::sim::kNullEntity};
   double velocity{0.2};
 
   void Configure(
-    const gz::sim::Entity &,
+    const ::gz::sim::Entity &,
     const std::shared_ptr<const sdf::Element> &_sdf,
-    gz::sim::EntityComponentManager &ecm,
-    gz::sim::EventManager &) override
+    ::gz::sim::EntityComponentManager &ecm,
+    ::gz::sim::EventManager &) override
   {
     std::cout << "[Conveyor] Configure START\n";
 
     if (_sdf && _sdf->HasElement("velocity"))
       this->velocity = _sdf->Get<double>("velocity");
 
-    ecm.Each<gz::sim::components::Name>(
-      [&](const gz::sim::Entity &_entity,
-          const gz::sim::components::Name *_name)
+    ecm.Each<::gz::sim::components::Name>(
+      [&](const ::gz::sim::Entity &_entity,
+          const ::gz::sim::components::Name *_name)
       {
         if (_name->Data() == "belt_moving")
         {
           this->beltLink = _entity;
-          std::cout << "[Conveyor] belt_moving FOUND: "
-                    << _entity << std::endl;
+          std::cout << "[Conveyor] belt_moving FOUND\n";
           return false;
         }
         return true;
       });
 
-    if (this->beltLink == gz::sim::kNullEntity)
+    if (this->beltLink == ::gz::sim::kNullEntity)
     {
       std::cout << "[Conveyor] belt_moving NOT FOUND\n";
       return;
@@ -54,36 +53,35 @@ public:
   }
 
   void PreUpdate(
-    const gz::sim::UpdateInfo &,
-    gz::sim::EntityComponentManager &ecm) override
+    const ::gz::sim::UpdateInfo &,
+    ::gz::sim::EntityComponentManager &ecm) override
   {
-    if (this->beltLink == gz::sim::kNullEntity)
+    if (this->beltLink == ::gz::sim::kNullEntity)
       return;
 
     auto velComp =
-      ecm.Component<gz::sim::components::LinearVelocityCmd>(this->beltLink);
+      ecm.Component<::gz::sim::components::LinearVelocityCmd>(this->beltLink);
 
     if (!velComp)
     {
       ecm.CreateComponent(
         this->beltLink,
-        gz::sim::components::LinearVelocityCmd(
+        ::gz::sim::components::LinearVelocityCmd(
           {0.0, this->velocity, 0.0}));
     }
     else
     {
-      velComp->Data()[0] = 0.0;
       velComp->Data()[1] = this->velocity;
-      velComp->Data()[2] = 0.0;
     }
   }
 };
 
+}
 
 
 GZ_ADD_PLUGIN(
   conveyor::ConveyorPlugin,
-  gz::sim::System,
-  gz::sim::ISystemConfigure,
-  gz::sim::ISystemPreUpdate
+  ::gz::sim::System,
+  ::gz::sim::ISystemConfigure,
+  ::gz::sim::ISystemPreUpdate
 )
