5608daf85cfa7bdcf64abd940ad87a087c8713fc
jepear19 <jesusper2010@gmail.com>
2026-03-26 19:55:37 +0100

    Fix spawn_box.py

--- CustomRobots/conveyor_belt/spawn_box.py
+++ CustomRobots/conveyor_belt/spawn_box.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python3
 
+import rclpy
+from rclpy.node import Node
 import subprocess
 import time
 
-i = 0
+class BoxSpawner(Node):
 
-while True:
-    name = f"box_{i}"
+    def __init__(self):
+        super().__init__('box_spawner')
 
-    cmd = [
-        "ros2", "run", "ros_gz_sim", "create",
-        "-name", name,
-        "-x", "0.0",
-        "-y", "0.0",
-        "-z", "1.0",
-        "-file", "model://box"
-    ]
+        time.sleep(5)
+        self.timer = self.create_timer(3.0, self.spawn_box)
+        self.counter = 0
 
-    subprocess.run(cmd)
-    print(f"Spawned {name}")
+    def spawn_box(self):
 
-    i += 1
-    time.sleep(2)
+        name = f"box_{self.counter}"
+
+        cmd = [
+            "ros2", "run", "ros_gz_sim", "create",
+            "-name", name,
+            "-x", "0.0",
+            "-y", "0.0",
+            "-z", "1.0",
+            "-file", "/home/ws/src/CustomRobots/conveyor_belt/box.sdf"
+        ]
+
+        subprocess.run(cmd, check=True)
+
+        self.get_logger().info(f"Spawned {name}")
+        self.counter += 1
+
+
+def main():
+    rclpy.init()
+    node = BoxSpawner()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
