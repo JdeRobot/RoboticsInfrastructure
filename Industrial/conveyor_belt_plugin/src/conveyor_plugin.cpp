fd4bf38eefdde5b1066e40f51b2dca9805e4e2f0
jepear19 <jesusper2010@gmail.com>
2026-03-26 19:32:11 +0100

    Fix plugin

--- Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
+++ Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
@@ -6,6 +6,8 @@
 
 #include <gz/plugin/Register.hh>
 
+#include <gz/math/Vector3.hh>
+
 #include <iostream>
 
 namespace conveyor
@@ -25,7 +27,7 @@ public:
   double min_y{-1.0}, max_y{1.0};
   double min_z{0.6}, max_z{1.0};
 
-  gz::math::Vector3d dir_vec{0,1,0};
+  gz::math::Vector3d dir_vec{0, 1, 0};
 
   void Configure(
     const gz::sim::Entity &,
@@ -33,29 +35,38 @@ public:
     gz::sim::EntityComponentManager &,
     gz::sim::EventManager &) override
   {
-    if (_sdf->HasElement("velocity"))
+    if (_sdf && _sdf->HasElement("velocity"))
       velocity = _sdf->Get<double>("velocity");
 
-    if (_sdf->HasElement("direction"))
+    if (_sdf && _sdf->HasElement("direction"))
       direction = _sdf->Get<std::string>("direction");
 
-    if (_sdf->HasElement("region"))
+    if (_sdf && _sdf->HasElement("region"))
     {
-      auto region = _sdf->GetElement("region");
+      auto region = _sdf->FindElement("region");
 
-      min_x = region->Get<double>("min_x");
-      max_x = region->Get<double>("max_x");
-      min_y = region->Get<double>("min_y");
-      max_y = region->Get<double>("max_y");
-      min_z = region->Get<double>("min_z");
-      max_z = region->Get<double>("max_z");
+      if (region)
+      {
+        if (region->HasElement("min_x")) min_x = region->Get<double>("min_x");
+        if (region->HasElement("max_x")) max_x = region->Get<double>("max_x");
+        if (region->HasElement("min_y")) min_y = region->Get<double>("min_y");
+        if (region->HasElement("max_y")) max_y = region->Get<double>("max_y");
+        if (region->HasElement("min_z")) min_z = region->Get<double>("min_z");
+        if (region->HasElement("max_z")) max_z = region->Get<double>("max_z");
+      }
     }
 
-    if (direction == "x") dir_vec = {velocity,0,0};
-    else if (direction == "y") dir_vec = {0,velocity,0};
-    else if (direction == "z") dir_vec = {0,0,velocity};
+    gz::math::Vector3d base_dir{0, 1, 0};
+
+    if (direction == "x") base_dir = {1, 0, 0};
+    else if (direction == "y") base_dir = {0, 1, 0};
+    else if (direction == "z") base_dir = {0, 0, 1};
+
+    dir_vec = base_dir * velocity;
 
     std::cout << "[Conveyor PRO] READY\n";
+    std::cout << "Velocity: " << velocity << std::endl;
+    std::cout << "Direction: " << direction << std::endl;
   }
 
   void PreUpdate(
@@ -64,11 +75,18 @@ public:
   {
     ecm.Each<
       gz::sim::components::Model,
-      gz::sim::components::Pose>(
+      gz::sim::components::Pose,
+      gz::sim::components::Name>(
       [&](const gz::sim::Entity &_entity,
           const gz::sim::components::Model *,
-          const gz::sim::components::Pose *_pose)
+          const gz::sim::components::Pose *_pose,
+          const gz::sim::components::Name *_name)
       {
+        const std::string &name = _name->Data();
+
+        if (name.find("box") == std::string::npos)
+          return true;
+
         auto pos = _pose->Data().Pos();
 
         if (pos.X() < min_x || pos.X() > max_x) return true;
@@ -86,7 +104,9 @@ public:
         }
         else
         {
-          vel->SetData(dir_vec);
+          vel->SetData(
+            dir_vec,
+            [](const auto &, const auto &){ return false; });
         }
 
         return true;
