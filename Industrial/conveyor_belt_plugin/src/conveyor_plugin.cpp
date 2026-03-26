3341bd4e753778d39eb21357010bcd3debe302c6
jepear19 <jesusper2010@gmail.com>
2026-03-26 12:27:30 +0100

    Fix contact plugin

--- Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
+++ Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
@@ -1,92 +1,96 @@
 #include <gz/sim/System.hh>
-#include <gz/sim/Model.hh>
-#include <gz/sim/components/JointVelocityCmd.hh>
-#include <gz/sim/components/JointForceCmd.hh>
+#include <gz/sim/components/Name.hh>
+#include <gz/sim/components/Pose.hh>
+#include <gz/sim/components/LinearVelocityCmd.hh>
+#include <gz/sim/components/LinearVelocity.hh>
+#include <gz/sim/components/WorldPose.hh>
+#include <gz/sim/components/CanonicalLink.hh>
 
 #include <gz/plugin/Register.hh>
 
 #include <iostream>
+#include <string>
 
 namespace conveyor
 {
 
-class ConveyorPlugin :
+class ConveyorSurfacePlugin :
   public ::gz::sim::System,
   public ::gz::sim::ISystemConfigure,
   public ::gz::sim::ISystemPreUpdate
 {
 public:
 
-  ::gz::sim::Model model{::gz::sim::kNullEntity};
-  ::gz::sim::Entity joint{::gz::sim::kNullEntity};
-
   double velocity{0.1};
 
+  double minX{-0.25}, maxX{0.25};
+  double minY{-0.6},  maxY{0.6};
+  double beltZ{0.75};
+  double zTolerance{0.05}; 
+
   void Configure(
-    const ::gz::sim::Entity &entity,
+    const ::gz::sim::Entity &,
     const std::shared_ptr<const sdf::Element> &_sdf,
-    ::gz::sim::EntityComponentManager &ecm,
+    ::gz::sim::EntityComponentManager &,
     ::gz::sim::EventManager &) override
   {
-    this->model = ::gz::sim::Model(entity);
-
-    this->joint = this->model.JointByName(ecm, "belt_joint");
-
     if (_sdf && _sdf->HasElement("velocity"))
       this->velocity = _sdf->Get<double>("velocity");
 
-    if (this->joint == ::gz::sim::kNullEntity)
-    {
-      std::cout << "[Conveyor] ❌ joint NOT FOUND\n";
-      return;
-    }
-
-    std::cout << "[Conveyor] ✅ READY (Joint control)\n";
+    std::cout << "[ConveyorSurface] READY\n";
   }
 
   void PreUpdate(
     const ::gz::sim::UpdateInfo &,
     ::gz::sim::EntityComponentManager &ecm) override
   {
-    if (this->joint == ::gz::sim::kNullEntity)
-      return;
+    ecm.Each<::gz::sim::components::Name,
+             ::gz::sim::components::WorldPose>(
+      [&](const ::gz::sim::Entity &_entity,
+          const ::gz::sim::components::Name *_name,
+          const ::gz::sim::components::WorldPose *_pose)
+      {
+        const std::string &n = _name->Data();
 
-    // Velocidad
-    auto velComp =
-      ecm.Component<::gz::sim::components::JointVelocityCmd>(this->joint);
+        if (n.find("box") == std::string::npos)
+          return true;
 
-    if (!velComp)
-    {
-      ecm.CreateComponent(
-        this->joint,
-        ::gz::sim::components::JointVelocityCmd({this->velocity}));
-    }
-    else
-    {
-      velComp->Data()[0] = this->velocity;
-    }
+        const auto &p = _pose->Data().Pos();
 
-    // Fuerza (importante)
-    auto forceComp =
-      ecm.Component<::gz::sim::components::JointForceCmd>(this->joint);
+        if (p.X() < minX || p.X() > maxX ||
+            p.Y() < minY || p.Y() > maxY)
+          return true;
 
-    if (!forceComp)
-    {
-      ecm.CreateComponent(
-        this->joint,
-        ::gz::sim::components::JointForceCmd({50.0}));
-    }
-    else
-    {
-      forceComp->Data()[0] = 50.0;
-    }
+        if (std::abs(p.Z() - beltZ) > zTolerance)
+          return true;
+
+        auto velCmd =
+          ecm.Component<::gz::sim::components::LinearVelocityCmd>(_entity);
+
+        if (!velCmd)
+        {
+          ecm.CreateComponent(
+            _entity,
+            ::gz::sim::components::LinearVelocityCmd(
+              {0.0, this->velocity, 0.0}));
+        }
+        else
+        {
+          velCmd->Data()[0] = 0.0;
+          velCmd->Data()[1] = this->velocity;
+          velCmd->Data()[2] = 0.0;
+        }
+
+        return true;
+      });
   }
 };
 
 }
 
+
 GZ_ADD_PLUGIN(
-  conveyor::ConveyorPlugin,
+  conveyor::ConveyorSurfacePlugin,
   ::gz::sim::System,
   ::gz::sim::ISystemConfigure,
   ::gz::sim::ISystemPreUpdate
