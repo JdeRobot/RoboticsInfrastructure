843ca16035cc7493cd7d486e0a4c5e923ac0ee0c
jepear19 <jesusper2010@gmail.com>
2026-03-29 00:38:02 +0100

    Fix movement

--- Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
+++ Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
@@ -1,4 +1,5 @@
 #include <gz/sim/System.hh>
+#include <gz/sim/Model.hh>
 #include <gz/sim/Entity.hh>
 
 #include <gz/sim/components/LinearVelocityCmd.hh>
@@ -29,16 +30,14 @@ public:
 
   gz::math::Vector3d dir_vec_{0,0,0};
 
-  bool debug_{false};
+  bool debug_{true};
 
   void Configure(
-      const gz::sim::Entity &/*_entity*/,
+      const gz::sim::Entity &_entity,
       const std::shared_ptr<const sdf::Element> &_sdf,
       gz::sim::EntityComponentManager &/*_ecm*/,
       gz::sim::EventManager &/*_eventMgr*/) override
   {
-    std::cout << "[Conveyor] Configurando plugin...\n";
-
     if (_sdf->HasElement("velocity"))
       velocity_ = _sdf->Get<double>("velocity");
 
@@ -52,7 +51,7 @@ public:
 
     if (!region)
     {
-      std::cerr << "[Conveyor][ERROR] No region definida\n";
+      std::cerr << "[Conveyor][ERROR] No region defined\n";
       return;
     }
 
@@ -71,12 +70,13 @@ public:
 
     if (debug_)
     {
-      std::cout << "[DEBUG] velocity: " << velocity_ << "\n";
-      std::cout << "[DEBUG] direction: " << direction_ << "\n";
-      std::cout << "[DEBUG] region: "
+      std::cout << "[Conveyor][DEBUG] velocity: " << velocity_ << std::endl;
+      std::cout << "[Conveyor][DEBUG] direction: " << direction_ << std::endl;
+      std::cout << "[Conveyor][DEBUG] region: "
                 << "X(" << min_x_ << "," << max_x_ << ") "
                 << "Y(" << min_y_ << "," << max_y_ << ") "
-                << "Z(" << min_z_ << "," << max_z_ << ")\n";
+                << "Z(" << min_z_ << "," << max_z_ << ")"
+                << std::endl;
     }
   }
 
@@ -88,7 +88,7 @@ public:
     counter++;
 
     if (debug_ && counter % 200 == 0)
-      std::cout << "[DEBUG] Update activo\n";
+      std::cout << "[Conveyor][DEBUG] Update loop activo\n";
 
     _ecm.Each<
       gz::sim::components::Pose,
@@ -98,57 +98,35 @@ public:
           const gz::sim::components::Name *_name)->bool
       {
         auto pos = _pose->Data().Pos();
-
-        bool inside =
-          pos.X() > min_x_ && pos.X() < max_x_ &&
-          pos.Y() > min_y_ && pos.Y() < max_y_ &&
-          pos.Z() > min_z_ && pos.Z() < max_z_;
-
-        auto velComp =
-          _ecm.Component<gz::sim::components::LinearVelocityCmd>(_entity);
-
-        if (inside)
+n
+        if (debug_ && counter % 500 == 0)
         {
-          if (debug_ && counter % 200 == 0)
+          std::cout << "[Entity] " << _name->Data()
+                    << " Pos: " << pos << std::endl;
+        }
+
+        if (pos.X() > min_x_ && pos.X() < max_x_ &&
+            pos.Y() > min_y_ && pos.Y() < max_y_ &&
+            pos.Z() > min_z_ && pos.Z() < max_z_)
+        {
+          if (debug_)
           {
-            std::cout << "[Conveyor] Dentro: "
-                      << _name->Data()
-                      << " Pos: " << pos << "\n";
+            std::cout << "[Conveyor] Dentro de región: "
+                      << _name->Data() << std::endl;
           }
 
-          gz::math::Vector3d newVel =
-            velComp ? velComp->Data() : gz::math::Vector3d(0,0,0);
-
-          if (direction_ == "x") newVel.X() = velocity_;
-          if (direction_ == "y") newVel.Y() = velocity_;
+          auto velComp =
+            _ecm.Component<gz::sim::components::LinearVelocityCmd>(_entity);
 
           if (!velComp)
           {
             _ecm.CreateComponent(
               _entity,
-              gz::sim::components::LinearVelocityCmd(newVel));
+              gz::sim::components::LinearVelocityCmd(dir_vec_ * velocity_));
           }
           else
           {
-            velComp->Data() = newVel;
-          }
-        }
-        else
-        {
-          if (velComp)
-          {
-            auto vel = velComp->Data();
-
-            if (direction_ == "x") vel.X() = 0;
-            if (direction_ == "y") vel.Y() = 0;
-
-            velComp->Data() = vel;
-
-            if (debug_ && counter % 200 == 0)
-            {
-              std::cout << "[Conveyor] Fuera: "
-                        << _name->Data() << "\n";
-            }
+            velComp->Data() = dir_vec_ * velocity_;
           }
         }
 
