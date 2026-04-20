5564b62b72a01a2cf93a326a2a489e9be4c86859
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-04-20 12:42:46 +0200

    Test with robot

--- Launchers/simple_circuit2.launch.py
+++ Launchers/simple_circuit2.launch.py
@@ -61,6 +61,6 @@ def generate_launch_description():
 
     ld.add_action(gazebo_server)
     ld.add_action(world_entity_cmd)
-    ld.add_action(spawn_robot_cmd)
+    # ld.add_action(spawn_robot_cmd)
 
     return ld
