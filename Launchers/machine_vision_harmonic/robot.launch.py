a4602c7ce731b7d650dd281081a3f94481e5ebd7
jepear19 <jesusper2010@gmail.com>
2026-05-01 17:49:25 +0200

    Migrate cameras to harmonic

--- Launchers/machine_vision_harmonic/robot.launch.py
+++ Launchers/machine_vision_harmonic/robot.launch.py
@@ -10,7 +10,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    pkg_path = get_package_share_directory("ur5_gripper_description")
+    pkg_path = get_package_share_directory("ros2srrc_ur5_gazebo")
 
     xacro_file = os.path.join(
         pkg_path,
