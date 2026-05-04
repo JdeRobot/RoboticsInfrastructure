f55faf74a733254360cbe66b7894654766a6def1
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-05-04 10:23:31 +0200

    Fix

--- database/universes.sql
+++ database/universes.sql
@@ -183,7 +183,7 @@ COPY public.universes (id, name, world_id, robot_id) FROM stdin;
 53 	Nurburgring Circuit Classic	53	0
 54 	Vacuums House Roof Classic	54	0
 55 	Rover 4wd Warehouse	55	0
-56	Pick And Place Harmonic World	56	0
+56	Pick And Place Harmonic World	56	1
 57 	Package delivery	57	0
 58	Warehouse 1 Harmonic	58	0
 59	Warehouse 2 Harmonic	59	0
@@ -255,6 +255,7 @@ COPY public.worlds (id, name, launch_file_path, tools_config, ros_version, type,
 54	Vacuums House Roof Classic	/opt/jderobot/Launchers/montecarlo_visual_loc_classic.launch.py	None	ROS2	gazebo	{0.0,0.0,0.0,0.0,0.0,0.0}
 55	Rover 4wd Warehouse	/opt/jderobot/Launchers/rover_4wd_warehouse.launch.py	None	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
 56	Pick And Place Harmonic	/opt/jderobot/Launchers/pick_place_harmonic/pick_place_harmonic.launch.py	{"rviz":"/opt/jderobot/Launchers/rviz/pick_place_harmonic.launch.py"}	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
+56	Pick And Place Harmonic	/opt/jderobot/Launchers/pick_place_harmonic.launch.py	{"rviz":"/opt/jderobot/Launchers/rviz/pick_place_harmonic.launch.py"}	ROS2	gz	{0.0,0.0,0.9,0.0,0.0,0.0}
 57	Package delivery world	/opt/jderobot/Launchers/package_delivery.launch.py	None	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
 58	Warehouse 1 Harmonic	/opt/jderobot/Launchers/warehouse1_amazon_robot_harmonic.launch.py	{"gzsim":"/opt/jderobot/Launchers/visualization/amazon_robot_harmonic.config"}	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
 59	Warehouse 2 Harmonic	/opt/jderobot/Launchers/warehouse2_amazon_robot_harmonic.launch.py	{"gzsim":"/opt/jderobot/Launchers/visualization/amazon_robot_harmonic.config"}	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
