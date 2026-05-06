c5974ffc73d2974a4a24d7a14659363467a673c6
aquintan <aquintana.camacho@proton.me>
2026-05-06 11:59:11 +0200

    make sim_time interface thread safe

--- common_interfaces_cpp/include/common_interfaces_cpp/hal/sim_time.hpp
+++ common_interfaces_cpp/include/common_interfaces_cpp/hal/sim_time.hpp
@@ -2,6 +2,7 @@
 #define COMMON_INTERFACES_CPP__SIM_TIME_HPP_
 
 #include <string>
+#include <mutex>
 #include "rclcpp/rclcpp.hpp"
 #include "rosgraph_msgs/msg/clock.hpp"
 
@@ -14,29 +15,25 @@ public:
     int nanoseconds;
 
     std::string to_string() const;
+    
+    double to_double() const; 
 };
 
-/*
- * Translates from ROS Clock to JderobotTypes SimTimeData.
- * @param clock: ROS Clock to translate
- * @type clock: Clock
- * @return a SimTimeData translated from clock
- */
 SimTimeData simTime2SimTimeData(const rosgraph_msgs::msg::Clock& clock);
-
 
 /* ### HAL INTERFACE ### */
 
 class SimTimeNode : public rclcpp::Node {
 public:
-    SimTimeNode();
-    SimTimeData getSimTime() const;
+    SimTimeNode(const std::string& node_name = "simulation_time_node");
+    SimTimeData getSimTime();
 
 private:
     void listener_callback(const rosgraph_msgs::msg::Clock::SharedPtr msg);
 
     rclcpp::Subscription<rosgraph_msgs::msg::Clock>::SharedPtr sub;
     rosgraph_msgs::msg::Clock last_sim_time_;
+    std::mutex lock_;
 };
 
 #endif
