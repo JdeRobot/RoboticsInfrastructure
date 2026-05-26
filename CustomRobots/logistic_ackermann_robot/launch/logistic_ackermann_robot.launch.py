44c53b4233f689bea79efb09d6b4e9735db1ccc3
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-05-26 10:29:36 +0200

    Add final ackermann robot

--- CustomRobots/logistic_ackermann_robot/launch/logistic_ackermann_robot.launch.py
+++ CustomRobots/logistic_ackermann_robot/launch/logistic_ackermann_robot.launch.py
@@ -27,26 +27,26 @@ def launch_setup(context):
     # =========================
     # ROBOT DESCRIPTION (URDF)
     # =========================
-    # xacro_file = os.path.join(
-    #     package_dir,
-    #     "models",
-    #     "logistic_ackermann_robot",
-    #     "logistic_ackermann_robot.urdf.xacro",
-    # )
-
-    # robot_description_content = xacro.process_file(
-    #     xacro_file
-    # ).toxml()
-
-    ## Temporary SDF load
-    sdf_file = os.path.join(
+    xacro_file = os.path.join(
         package_dir,
         "models",
         "logistic_ackermann_robot",
-        "logistic_ackermann_robot.sdf",
+        "logistic_ackermann_robot.urdf.xacro",
     )
-    with open(sdf_file, "r") as infp:
-        robot_description_content = infp.read()
+
+    robot_description_content = xacro.process_file(
+        xacro_file
+    ).toxml()
+
+    ## Temporary SDF load
+    # sdf_file = os.path.join(
+    #     package_dir,
+    #     "models",
+    #     "logistic_ackermann_robot",
+    #     "logistic_ackermann_robot.sdf",
+    # )
+    # with open(sdf_file, "r") as infp:
+    #     robot_description_content = infp.read()
 
     robot_description = {"robot_description": robot_description_content}
 
