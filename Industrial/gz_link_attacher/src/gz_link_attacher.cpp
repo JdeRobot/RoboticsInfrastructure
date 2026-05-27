ad843a094e441d656c69435084c13753406e5564
jepear19 <jesusper2010@gmail.com>
2026-05-27 12:28:47 +0200

    Change collision sensor

--- Industrial/gz_link_attacher/src/gz_link_attacher.cpp
+++ Industrial/gz_link_attacher/src/gz_link_attacher.cpp
@@ -117,12 +117,12 @@ void Configure(
   std::cout << "[LinkAttacher] Subscribing to contact topics" << std::endl;
 
   gzNode.Subscribe(
-    "/world/default/model/ur5_robotiq/link/robotiq_85_left_finger_tip_link/sensor/left_finger_contact/contact",
+    "/left_finger_contact",
     &LinkAttacher::OnContact,
     this);
 
   gzNode.Subscribe(
-    "/world/default/model/ur5_robotiq/link/robotiq_85_right_finger_tip_link/sensor/right_finger_contact/contact",
+    "/right_finger_contact",
     &LinkAttacher::OnContact,
     this);
 
