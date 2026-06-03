f350873a00ea321916b9961dd367bf85b9256d9f
jepear19 <jesusper2010@gmail.com>
2026-06-03 09:43:31 +0200

    Add plugins path to world launcher

--- Launchers/conveyor/world.launch.py
+++ Launchers/conveyor/world.launch.py
@@ -1,26 +1,71 @@
 import os
+
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 
 
 def generate_launch_description():
 
     world_path = "/opt/jderobot/Worlds/conveyor_world.world"
 
-    gz_env = {
-        "DISPLAY": ":2",
-        "GZ_SIM_RESOURCE_PATH": "/home/ws/src/CustomRobots",
-        "GZ_SIM_SYSTEM_PLUGIN_PATH": "/home/ws/install/conveyor_belt_plugin/lib:/opt/ros/humble/lib",
-        "LD_LIBRARY_PATH": "/home/ws/install/conveyor_belt_plugin/lib:/opt/ros/humble/lib:/usr/lib/x86_64-linux-gnu",
-    }
+    # ==================================================
+    # PLUGIN PATHS
+    # ==================================================
+
+    gz_ros2_control_path = "/home/ws/install/gz_ros2_control/lib"
+    gz_link_attacher_path = "/home/ws/install/gz_link_attacher/lib"
+    conveyor_plugin_path = "/home/ws/install/conveyor_belt_plugin/lib"
+
+    gz_plugin_path = (
+        conveyor_plugin_path
+        + ":"
+        + gz_link_attacher_path
+        + ":"
+        + gz_ros2_control_path
+        + ":"
+        + "/opt/ros/humble/lib"
+    )
+
+    # ==================================================
+    # RESOURCE PATHS
+    # ==================================================
+
+    resource_path = (
+        "/home/ws/src/CustomRobots"
+    )
+
+    set_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=resource_path,
+    )
+
+    set_gz_plugin_path = SetEnvironmentVariable(
+        name="GZ_SIM_SYSTEM_PLUGIN_PATH",
+        value=gz_plugin_path,
+    )
+
+    existing_ld = os.environ.get("LD_LIBRARY_PATH", "")
+
+    set_ld_library_path = SetEnvironmentVariable(
+        name="LD_LIBRARY_PATH",
+        value=gz_plugin_path
+        + ":/usr/lib/x86_64-linux-gnu:"
+        + existing_ld,
+    )
+
+    # ==================================================
+    # GAZEBO
+    # ==================================================
 
     gazebo = ExecuteProcess(
         cmd=["gz", "sim", "-r", "-v", "4", world_path],
         output="screen",
-        additional_env=gz_env,
         shell=False,
     )
 
     return LaunchDescription([
+        set_gz_plugin_path,
+        set_ld_library_path,
+        set_resource_path,
         gazebo,
     ])
