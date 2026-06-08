7d409c4819826ed039f34bf85a62eeed16121b75
Ashwani1330 <ashwanihabri@gmail.com>
2026-06-09 01:00:50 +0530

    feat: Add palletizing exercise (exercise 27, universe 71)
    - Exercise templates (Python + C++ HAL, WebGUI) copied from pick_place
    - DB: exercise 27 with tools (console, simulator, rviz) linked to universe 71

---
+++ Launchers/palletizing.launch.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import os
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    world_file_name = "palletizing_arm_harmonic.world"
+    worlds_dir = "/opt/jderobot/Worlds"
+    world_path = os.path.join(worlds_dir, world_file_name)
+
+    package_dir = get_package_share_directory("custom_robots")
+    gazebo_models_path = os.path.join(package_dir, "models")
+
+    # Paths
+    gz_ros2_control_path = "/home/ws/install/gz_ros2_control/lib"
+    gz_link_attacher_path = "/home/ws/install/gz_link_attacher/lib"
+
+    gz_plugin_path = (
+        gz_link_attacher_path + ":" + gz_ros2_control_path + ":" + "/opt/ros/humble/lib"
+    )
+
+    resource_path = (
+        os.path.dirname(get_package_share_directory("ros2srrc_ur5_gazebo"))
+        + ":"
+        + os.path.dirname(get_package_share_directory("robotiq_description"))
+        + ":"
+        + gazebo_models_path
+    )
+
+    set_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH", value=resource_path
+    )
+
+    set_gz_plugin_path = SetEnvironmentVariable(
+        name="GZ_SIM_SYSTEM_PLUGIN_PATH", value=gz_plugin_path
+    )
+
+    existing_ld = os.environ.get("LD_LIBRARY_PATH", "")
+
+    set_ld_library_path = SetEnvironmentVariable(
+        name="LD_LIBRARY_PATH",
+        value=gz_plugin_path + ":/usr/lib/x86_64-linux-gnu:" + existing_ld,
+    )
+
+    gz = ExecuteProcess(
+        cmd=["gz", "sim", "-r", "-s", "-v", "4", world_path],
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [set_gz_plugin_path, set_ld_library_path, set_resource_path, gz]
+    )
