d3678415d82456dbec1427917a5f7b5de118ad51
jepear19 <jesusper2010@gmail.com>
2026-06-18 07:23:49 +0200

    Add Rviz launcher

--- Launchers/rviz/sausage_exercise.launch.py
+++ Launchers/rviz/sausage_exercise.launch.py
@@ -6,12 +6,18 @@ Assumes Gazebo and robot are already running
 
 import os
 import xacro
+import yaml
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import TimerAction
 
 from ament_index_python.packages import get_package_share_directory
+
+def load_yaml(package_name, file_path):
+    pkg_path = get_package_share_directory(package_name)
+    with open(os.path.join(pkg_path, file_path), "r") as f:
+        return yaml.safe_load(f)
 
 
 def generate_launch_description():
@@ -23,6 +29,7 @@ def generate_launch_description():
     moveit_pkg_share = get_package_share_directory(
         "ros2srrc_ur3_moveit2"
     )
+    
 
     gazebo_pkg_share = get_package_share_directory(
         "ros2srrc_ur3_gazebo"
@@ -76,19 +83,19 @@ def generate_launch_description():
     # MoveIt Config Files
     # =====================================================
 
-    kinematics_yaml = os.path.join(
-        get_package_share_directory("ros2srrc_robots"),
-        "ur3",
-        "config",
-        "kinematics.yaml",
+    kinematics_yaml = {
+        "robot_description_kinematics": load_yaml(
+            "ros2srrc_robots",
+            "ur3/config/kinematics.yaml",
+        )
+    }
+
+    ompl_planning = load_yaml(
+        "ros2srrc_robots",
+        "ur3/config/ompl_planning.yaml",
     )
 
-    ompl_planning_yaml = os.path.join(
-        get_package_share_directory("ros2srrc_robots"),
-        "ur3",
-        "config",
-        "ompl_planning.yaml",
-    )
+    ompl_planning = ompl_planning["/**"]["ros__parameters"]
 
     # =====================================================
     # RViz Config
@@ -109,7 +116,7 @@ def generate_launch_description():
         parameters=[
             robot_description,
             robot_description_semantic,
-            ompl_planning_yaml,
+            ompl_planning,
             kinematics_yaml,
             {"use_sim_time": True},
         ],
