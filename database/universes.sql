3152728665aa1b8fe1f58dbf01044a6fbee9c60b
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-06-22 11:41:08 +0200

    Update db

--- database/universes.sql
+++ database/universes.sql
@@ -156,7 +156,7 @@ COPY public.universes (id, name, world_id, robot_id) FROM stdin;
 26	Autoparking Lidar Simple: In line	5	0
 27	Vacuums House Roof	26	10
 28	Restaurant	21	0
-29	Vacuums House Markers	25	17
+29	Vacuums House Markers	25	21
 30	Small Laser Mapping Warehouse	33	21
 31	Rescue People Harmonic	31	0
 32	Follow Road Harmonic	32	0
@@ -198,6 +198,7 @@ COPY public.universes (id, name, world_id, robot_id) FROM stdin;
 68	Rover 4wd Warehouse Low Noise	68	19
 69	Rover 4wd Warehouse High Noise	68	20
 70	Machine vision Harmonic world	70	24
+71	Conveyor Belt World	71	0
 \.
 
 --
@@ -264,6 +265,7 @@ COPY public.worlds (id, name, launch_file_path, tools_config, ros_version, type,
 67	Follow Person Teleop Harmonic	/opt/jderobot/Launchers/follow_person_teleop_harmonic.launch.py	{"gzsim":"/opt/jderobot/Launchers/visualization/follow_person.config"}	ROS2	gz	{-1.0,10.0,0.1,0.0,0.0,0.0}
 68	Rover 4wd Warehouse Low Noise	/opt/jderobot/Launchers/rover_4wd_warehouse.launch.py	None	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
 70	Machine Vision Harmonic	/opt/jderobot/Launchers/machine_vision.launch.py	{"rviz":"/opt/jderobot/Launchers/rviz/pick_place_harmonic.launch.py"}	ROS2	gz	{0.0,0.0,0.9,0.0,0.0,0.0}
+71	Conveyor Harmonic	/opt/jderobot/Launchers/sausage_exercise.launch.py	None	ROS2	gz	{0.0,0.0,0.0,0.0,0.0,0.0}
 \.
 
 --
