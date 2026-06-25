2c24b1818bf75083c612ef891b38f57331734a72
jepear19 <jesusper2010@gmail.com>
2026-06-26 00:18:28 +0200

    Fix rviz launcher

--- Launchers/rviz/sausage_exercise.launch.py
+++ Launchers/rviz/sausage_exercise.launch.py
@@ -26,37 +26,40 @@ def generate_launch_description():
     # Packages
     # =====================================================
 
-    moveit_pkg_share = get_package_share_directory(
-        "ros2srrc_ur3_moveit2"
-    )
-    
-
-    gazebo_pkg_share = get_package_share_directory(
-        "ros2srrc_ur3_gazebo"
-    )
+    pkg_share_dir = get_package_share_directory("custom_robots")
+    moveit_pkg_share = get_package_share_directory("ros2srrc_ur3_moveit2")
 
     # =====================================================
     # URDF
     # =====================================================
 
     xacro_file = os.path.join(
-        gazebo_pkg_share,
-        "urdf",
-        "ur3_robotiq_2f85.urdf.xacro",
+        pkg_share_dir,
+        "models",
+        "ur3",
+        "ur3.urdf.xacro",
+    )
+
+    controllers_file = os.path.join(
+        pkg_share_dir,
+        "config",
+        "ur3_controllers.yaml",
     )
 
     robot_description_content = xacro.process_file(
         xacro_file,
         mappings={
-            "bringup": "false",
+            "ur_type": "ur3",
+            "name": "ur",
+            "prefix": "",
+            "use_fake_hardware": "false",
+            "sim_gazebo": "false",
+            "sim_gz": "true",
+            "simulation_controllers": controllers_file,
             "hmi": "false",
-            "robot_ip": "0.0.0.0",
             "EE": "true",
             "EE_name": "robotiq_2f85",
             "camera": "false",
-            "script_filename": "none",
-            "input_recipe_filename": "none",
-            "output_recipe_filename": "none",
         },
     ).toxml()
 
@@ -83,11 +86,14 @@ def generate_launch_description():
     # MoveIt Config Files
     # =====================================================
 
+    kinematics_yaml = load_yaml(
+        "ur3_gripper_moveit_config",
+        "config/kinematics.yaml",
+    )
+
     kinematics_yaml = {
-        "robot_description_kinematics": load_yaml(
-            "ros2srrc_robots",
-            "ur3/config/kinematics.yaml",
-        )
+        "robot_description_kinematics":
+            kinematics_yaml["/**"]["ros__parameters"]
     }
 
     ompl_planning = load_yaml(
