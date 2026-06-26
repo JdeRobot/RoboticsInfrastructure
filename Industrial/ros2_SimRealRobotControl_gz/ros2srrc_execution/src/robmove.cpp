7bfb22a456bf520cfaac9fa98c35a32c6aa2fe98
jepear19 <jesusper2010@gmail.com>
2026-06-26 10:11:34 +0200

    add more debug to robmove.cpp

--- Industrial/ros2_SimRealRobotControl_gz/ros2srrc_execution/src/robmove.cpp
+++ Industrial/ros2_SimRealRobotControl_gz/ros2srrc_execution/src/robmove.cpp
@@ -239,7 +239,7 @@ private:
                 ik_joints[i]);
         }
 
-        move_group_interface_ROB.setJointValueTarget(*state);
+        move_group_interface_ROB.setJointValueTarget(ik_joints);
 
         auto current_state = move_group_interface_ROB.getCurrentState();
 
@@ -278,6 +278,23 @@ private:
                     i,
                     start[i]);
             }
+        }
+
+        moveit_msgs::msg::Constraints goal =
+            move_group_interface_ROB.getGoalConstraints();
+
+        RCLCPP_INFO(
+            get_logger(),
+            "Goal constraints: %zu joint constraints",
+            goal.joint_constraints.size());
+
+        for (const auto &jc : goal.joint_constraints)
+        {
+            RCLCPP_INFO(
+                get_logger(),
+                "%s -> %.6f",
+                jc.joint_name.c_str(),
+                jc.position);
         }
 
         MyPlan = plan_ROB();
