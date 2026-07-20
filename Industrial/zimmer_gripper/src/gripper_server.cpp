431b767a13b1f2a00a64b7d847b1f0aefa3f25d8
jepear19 <jesusper2010@gmail.com>
2026-07-20 12:27:38 +0200

    Add more methods to node

--- Industrial/zimmer_gripper/src/gripper_server.cpp
+++ Industrial/zimmer_gripper/src/gripper_server.cpp
@@ -1,5 +1,7 @@
 #include "zimmer_gripper/gripper_server.hpp"
 
+#include <chrono>
+#include <future>
 #include <thread>
 
 using namespace std::placeholders;
@@ -7,26 +9,24 @@ using namespace std::placeholders;
 ZimmerGripperServer::ZimmerGripperServer()
 : Node("zimmer_gripper_server")
 {
-    // Cliente del servicio SetIO
-    io_client_ =
-        this->create_client<ur_msgs::srv::SetIO>(
-            "/io_and_status_controller/set_io");
+  io_client_ =
+      this->create_client<ur_msgs::srv::SetIO>(
+          "/io_and_status_controller/set_io");
 
-    while (!io_client_->wait_for_service(std::chrono::seconds(1)))
-    {
-        RCLCPP_INFO(get_logger(),
-                    "Esperando al servicio /io_and_status_controller/set_io...");
-    }
+  while (!io_client_->wait_for_service(std::chrono::seconds(1))) {
+    RCLCPP_INFO(
+        this->get_logger(),
+        "Esperando al servicio /io_and_status_controller/set_io...");
+  }
 
-    // Crear Action Server
-    action_server_ = rclcpp_action::create_server<Gripper>(
-        this,
-        "zimmer_gripper",
-        std::bind(&ZimmerGripperServer::handle_goal, this, _1, _2),
-        std::bind(&ZimmerGripperServer::handle_cancel, this, _1),
-        std::bind(&ZimmerGripperServer::handle_accepted, this, _1));
+  action_server_ = rclcpp_action::create_server<Gripper>(
+      this,
+      "zimmer_gripper",
+      std::bind(&ZimmerGripperServer::handle_goal, this, _1, _2),
+      std::bind(&ZimmerGripperServer::handle_cancel, this, _1),
+      std::bind(&ZimmerGripperServer::handle_accepted, this, _1));
 
-    RCLCPP_INFO(get_logger(), "Zimmer Gripper Action Server iniciado.");
+  RCLCPP_INFO(this->get_logger(), "Zimmer Gripper Server iniciado");
 }
 
 rclcpp_action::GoalResponse
@@ -34,26 +34,116 @@ ZimmerGripperServer::handle_goal(
     const rclcpp_action::GoalUUID &,
     std::shared_ptr<const Gripper::Goal>)
 {
-    RCLCPP_INFO(get_logger(), "Nuevo objetivo recibido.");
-
-    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  RCLCPP_INFO(this->get_logger(), "Objetivo recibido");
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 }
 
 rclcpp_action::CancelResponse
 ZimmerGripperServer::handle_cancel(
     const std::shared_ptr<GoalHandleGripper>)
 {
-    RCLCPP_INFO(get_logger(), "Cancelación recibida.");
-
-    return rclcpp_action::CancelResponse::ACCEPT;
+  RCLCPP_INFO(this->get_logger(), "Cancelación recibida");
+  return rclcpp_action::CancelResponse::ACCEPT;
 }
 
 void ZimmerGripperServer::handle_accepted(
     const std::shared_ptr<GoalHandleGripper> goal_handle)
 {
-    std::thread(
-        std::bind(&ZimmerGripperServer::execute,
-                  this,
-                  goal_handle))
-        .detach();
+  std::thread(
+      std::bind(
+          &ZimmerGripperServer::execute,
+          this,
+          goal_handle))
+      .detach();
+}
+
+void ZimmerGripperServer::execute(
+    const std::shared_ptr<GoalHandleGripper> goal_handle)
+{
+  const auto goal = goal_handle->get_goal();
+
+  auto result = std::make_shared<Gripper::Result>();
+
+  bool ok;
+
+  if (goal->open) {
+    RCLCPP_INFO(this->get_logger(), "Abriendo gripper");
+    ok = openGripper();
+  } else {
+    RCLCPP_INFO(this->get_logger(), "Cerrando gripper");
+    ok = closeGripper();
+  }
+
+  result->success = ok;
+
+  if (ok) {
+    goal_handle->succeed(result);
+  } else {
+    goal_handle->abort(result);
+  }
+}
+
+bool ZimmerGripperServer::setToolOutput(uint8_t pin, bool state)
+{
+  auto request = std::make_shared<ur_msgs::srv::SetIO::Request>();
+
+  request->fun = ur_msgs::srv::SetIO::Request::FUN_SET_DIGITAL_OUT;
+  request->pin = pin;
+  request->state =
+      state ?
+      ur_msgs::srv::SetIO::Request::STATE_ON :
+      ur_msgs::srv::SetIO::Request::STATE_OFF;
+
+  auto future = io_client_->async_send_request(request);
+
+  if (future.wait_for(std::chrono::seconds(2)) !=
+      std::future_status::ready) {
+    RCLCPP_ERROR(this->get_logger(), "Timeout llamando al servicio SetIO");
+    return false;
+  }
+
+  return future.get()->success;
+}
+
+bool ZimmerGripperServer::openGripper()
+{
+  bool ok = true;
+
+  ok &= setToolOutput(
+      ur_msgs::srv::SetIO::Request::PIN_TOOL_DOUT1,
+      false);
+
+  ok &= setToolOutput(
+      ur_msgs::srv::SetIO::Request::PIN_TOOL_DOUT0,
+      true);
+
+  return ok;
+}
+
+bool ZimmerGripperServer::closeGripper()
+{
+  bool ok = true;
+
+  ok &= setToolOutput(
+      ur_msgs::srv::SetIO::Request::PIN_TOOL_DOUT0,
+      false);
+
+  ok &= setToolOutput(
+      ur_msgs::srv::SetIO::Request::PIN_TOOL_DOUT1,
+      true);
+
+  return ok;
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<ZimmerGripperServer>();
+
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+
+  return 0;
 }
