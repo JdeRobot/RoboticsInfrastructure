5d43fb0568c26c9add8db23767f7c61626f84991
anish <b24331@students.iitmandi.ac.in>
2026-07-22 22:01:59 +0530

    solving color issue

--- CustomRobots/quadrotor/launch/quadrotor.launch.py
+++ CustomRobots/quadrotor/launch/quadrotor.launch.py
@@ -25,6 +25,7 @@ def launch_setup(context):
     gz_sensor = LaunchConfiguration("sensor")
     gz_namespace = LaunchConfiguration("namespace")
     gz_entity = LaunchConfiguration("entity")
+    gz_color = LaunchConfiguration("color")
 
     package_dir = get_package_share_directory("custom_robots")
 
@@ -33,6 +34,7 @@ def launch_setup(context):
     sensor = gz_sensor.perform(context)
     namespace = gz_namespace.perform(context)
     entity = gz_entity.perform(context)
+    color = gz_color.perform(context)
 
     # =========================
     # ROBOT DESCRIPTION (URDF)
@@ -49,6 +51,7 @@ def launch_setup(context):
         mappings={
             "camera": "true" if sensor == "camera" else "false",
             "namespace": namespace,
+            "color": color,
         },
     ).toxml()
 
@@ -171,6 +174,7 @@ def generate_launch_description():
         DeclareLaunchArgument("sensor", default_value="camera"),
         DeclareLaunchArgument("namespace", default_value="drone"),
         DeclareLaunchArgument("entity", default_value="drone"),
+        DeclareLaunchArgument("color", default_value="Blue"),
     ]
 
     return LaunchDescription(
