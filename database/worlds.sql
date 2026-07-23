b9ad8f303800b38d3b019b177fa5bd06c313473b
anish <b24331@students.iitmandi.ac.in>
2026-07-23 13:21:04 +0530

    adding color argument

--- database/worlds.sql
+++ database/worlds.sql
@@ -253,7 +253,8 @@ COPY public.worlds_robots (id, world_id, robot_id, poses) FROM stdin;
 43	69	20	{{0.0,0.0,0.0,0.0,0.0,0.0}}
 44	70	24	{{0.0,0.0,0.9,0.0,0.0,0.0}}
 45	71	26	{{-0.4,-0.5,0.8,0.0,0.0,0.0}}
-46	72	11	{{0,5,0.2,0.0,0.0,0.0},{20,5,0.2,0.0,0.0,0.0}}
+46	72	31	{{0,5,0,0.0,0.0,0.0}}
+47	72	32	{{20,5,0,0.0,0.0,0.0}}
 \.
 
 --
@@ -333,6 +334,8 @@ COPY public.robots (id, name, launch_file_path, entity, extra_config, model_path
 28	Dingo Low Noise	/home/ws/src/CustomRobots/dingo/launch/dingo.launch.py	do150	noise:=low namespace:=do150	dingo/model/dingo/dingo.urdf.xacro
 29	Dingo Medium Noise	/home/ws/src/CustomRobots/dingo/launch/dingo.launch.py	do150	noise:=med namespace:=do150	dingo/model/dingo/dingo.urdf.xacro
 30	Dingo High Noise	/home/ws/src/CustomRobots/dingo/launch/dingo.launch.py	do150	noise:=high namespace:=do150	dingo/model/dingo/dingo.urdf.xacro
+31	Quadrotor Cat	/home/ws/src/CustomRobots/quadrotor/launch/quadrotor.launch.py	drone	sensor:=camera namespace:=drone color:=Orange	quadrotor/models/quadrotor/quadrotor.urdf.xacro
+32	Quadrotor Mouse	/home/ws/src/CustomRobots/quadrotor/launch/quadrotor.launch.py	drone_mouse	sensor:=camera namespace:=drone_mouse color:=Majenta	quadrotor/models/quadrotor/quadrotor.urdf.xacro
 \.
 
 --
