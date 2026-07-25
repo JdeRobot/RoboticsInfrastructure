5541a23296d0074f1e268e90bad69a759b4135e7
anish <b24331@students.iitmandi.ac.in>
2026-07-25 14:13:13 +0530

    adding scale

--- database/worlds.sql
+++ database/worlds.sql
@@ -334,7 +334,7 @@ COPY public.robots (id, name, launch_file_path, entity, extra_config, model_path
 28	Dingo Low Noise	/home/ws/src/CustomRobots/dingo/launch/dingo.launch.py	do150	noise:=low namespace:=do150	dingo/model/dingo/dingo.urdf.xacro
 29	Dingo Medium Noise	/home/ws/src/CustomRobots/dingo/launch/dingo.launch.py	do150	noise:=med namespace:=do150	dingo/model/dingo/dingo.urdf.xacro
 30	Dingo High Noise	/home/ws/src/CustomRobots/dingo/launch/dingo.launch.py	do150	noise:=high namespace:=do150	dingo/model/dingo/dingo.urdf.xacro
-31	Quadrotor Cat	/home/ws/src/CustomRobots/quadrotor/launch/quadrotor.launch.py	drone	sensor:=camera namespace:=drone color:=Orange scale:=3.0	quadrotor/models/quadrotor/quadrotor.urdf.xacro
+31	Quadrotor Cat	/home/ws/src/CustomRobots/quadrotor/launch/quadrotor.launch.py	drone	sensor:=camera namespace:=drone scale:=3.0	quadrotor/models/quadrotor/quadrotor.urdf.xacro
 32	Quadrotor Mouse	/home/ws/src/CustomRobots/quadrotor/launch/quadrotor.launch.py	drone_mouse	sensor:=camera namespace:=drone_mouse color:=Magenta scale:=3.0	quadrotor/models/quadrotor/quadrotor.urdf.xacro
 \.
 
