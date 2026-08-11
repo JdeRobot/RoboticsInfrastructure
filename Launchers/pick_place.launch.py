4c16d6953bd8e787b0c520213aa16bbba558655c
jepear19 <jesusper2010@gmail.com>
2026-08-11 08:30:04 +0200

    Add ROS Node import to pick and place and machine vision launchers

--- Launchers/pick_place.launch.py
+++ Launchers/pick_place.launch.py
@@ -4,6 +4,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
