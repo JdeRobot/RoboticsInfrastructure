280aaaf1de3737141103bdf36ca1255e7c82fe51
jepear19 <jesusper2010@gmail.com>
2026-09-01 18:45:24 +0200

    Change spawn_sauage.py to use sim time instead of real time

--- Launchers/sausage_exercise.launch.py
+++ Launchers/sausage_exercise.launch.py
@@ -104,7 +104,7 @@ def generate_launch_description():
     sausage_spawner = ExecuteProcess(
         cmd=[
             "python3",
-            "home/ws/src/CustomRobots/conveyor_belt/spawn_sausage.py",
+            "/home/ws/src/CustomRobots/conveyor_belt/spawn_sausage.py",
             "--ros-args",
             "-p",
             "use_sim_time:=true",
