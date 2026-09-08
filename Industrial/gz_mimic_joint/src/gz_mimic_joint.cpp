212b7a3deb31816bef7b40cb1835a25ceaabc192
aquintan <aquintana.camacho@proton.me>
2026-09-08 13:30:39 +0200

    mimic joint

--- Industrial/gz_mimic_joint/src/gz_mimic_joint.cpp
+++ Industrial/gz_mimic_joint/src/gz_mimic_joint.cpp
@@ -2,6 +2,7 @@
 #include <gz/sim/EntityComponentManager.hh>
 #include <gz/sim/components/Name.hh>
 #include <gz/sim/components/JointPosition.hh>
+#include <gz/sim/components/JointPositionReset.hh>
 
 #include <gz/plugin/Register.hh>
 
@@ -70,7 +71,10 @@ void PreUpdate(
 
     if (parentPos && !parentPos->Data().empty())
     {
-      _ecm.SetComponentData<components::JointPosition>(
+      // JointPosition is Physics' own output, writing it here just gets
+      // overwritten again once Physics steps. JointPositionReset is the
+      // component Physics actually reads to force a joint to a value.
+      _ecm.SetComponentData<components::JointPositionReset>(
         mimicJoint,
         {parentPos->Data()[0]}
       );
